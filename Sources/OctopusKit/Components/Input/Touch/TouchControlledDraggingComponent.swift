a1fd0842d41dcad695895bb3c8a59d4fbe84f668
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2018-07-17 21:44:53 +0400

    Fix and update TouchControlledDraggingComponent
    - Fix behavior in nested nodes by checking position in parent instead of scene.
    - Add TouchControlledDraggingComponent.isDragging
    - Ensure component cannot be added to the scene entity.
    - Ensure flags are reset when removed from entity.
    - Improve comments

--- Sources/OctopusKit/Components/Input/Touch/TouchControlledDraggingComponent.swift
+++ Sources/OctopusKit/Components/Input/Touch/TouchControlledDraggingComponent.swift
@@ -7,20 +7,18 @@
 //
 
 // CHECK: Enforce `isUserInteractionEnabled`?
+// CHECK: Add an option for `force` sensitivity?
+// CHECK: Add an option for "dead zone" radius tolerance?
+// CHECK: Behavior on physics bodies.
 
 // CHECKED: This component works whether the `TouchEventComponent` is added to a sprite entity or the scene entity (via a `RelayComponent`) :)
 
-// CHECK: Add a `force` option?
-// CHECK: Add an optional "dead zone" radius tolerance?
-// CHECK: Behavior on physics bodies.
-
-// TODO: Camera movement support
-
-// TODO: BUG: Fix multitouch handling even if not tracking multiple touches. (BUG 20180504B)
+// TODO: BUG: Fix multitouch handling even if not tracking multiple touches. (BUG 20180504B, RADAR 39997859)
+// TODO: FIX or WARN: Using `initialNodePosition` to determine the final position at the end of the dragging gesture does not account for changes in position by external forces during the gesture (e.g. physics.)
 
 // PERFORMANCE: May need optimization?
 
-// 💬 It's possible to make the node move only when the touch moves out through its edges, by writing:
+// 💬 NOTE: It's possible to make the node move only when the touch moves out through its edges, by writing:
 //
 //  let nodePositionAndCenterDelta = node.position - node.frame.center
 //  node.position = (currentTouchLocation - initialNodeCenterAndTouchPositionDelta) + nodePositionAndCenterDelta
@@ -34,7 +32,7 @@ import GameplayKit
 
 /// Allows the player to drag the entity's `SpriteKitComponent` node based on input from the entity's `NodeTouchComponent`.
 ///
-/// **Dependencies:** `SpriteKitComponent`, `NodeTouchComponent`
+/// **Dependencies:** `NodeTouchComponent, SpriteKitComponent`
 public final class TouchControlledDraggingComponent: OctopusComponent, OctopusUpdatableComponent {
     
     public override var requiredComponents: [GKComponent.Type]? {
@@ -42,37 +40,46 @@ public final class TouchControlledDraggingComponent: OctopusComponent, OctopusUp
                 NodeTouchComponent.self]
     }
     
-    /// Stores the initial position of the node, to compare against the `TouchStateComponent`'s translation over time.
+    /// Stores the initial position of the node to compare against the `TouchStateComponent`'s translation over time.
     private var initialNodePosition: CGPoint?
     
-    /// If `true`, the node is only moved to the touch's location when the touch moves, i.e. the touch's timestamp delta is greater than 0, but the node will drift away from the touch when the camera or the node's parent moves.
+    /// If `true`, which is the default, the node is only moved to the touch's location when the touch moves, i.e. when the touch's timestamp delta is greater than `0`, but the node will drift away from the touch when the camera or the node's parent moves.
     ///
-    /// If `false`, the node's location is always updated to match the touch's location, even if the touch is stationary. This produced the correct and expected visual behavior in cases such as a moving camera, but may decrease performance.
+    /// If `false`, the node's location is always updated to match the touch's location, even if the touch is stationary. This produced the correct and expected behavior in cases such as a moving camera, but may decrease performance.
     public var onlyMoveWhenTouchTimestampChanges: Bool = true
+    
+    public var isDragging: Bool = false {
+        didSet {
+            #if LOGINPUT
+            if isDragging != oldValue { debugLog("= \(oldValue) → \(isDragging)") }
+            #endif
+        }
+    }
     
     public override func update(deltaTime seconds: TimeInterval) {
         
-        // #1: Do we have a node, a scene, and are we tracking a touch?
+        // #1: Do we have a node, which has a parent, and are we tracking a touch?
         
         guard
             let node = self.entityNode,
-            let scene = node.scene,
+            let parent = node.parent,
             let nodeTouchComponent = coComponent(NodeTouchComponent.self),
             let trackedTouch = nodeTouchComponent.trackedTouch
             else {
                 initialNodePosition = nil
+                isDragging = false
                 return
         }
         
-        let currentTouchLocation = trackedTouch.location(in: scene) // Used for debugging log and ahead.
+        // PERFORMANCE: Cache the touch component's properties locally so that we don't have to query another class's properties too much. CHECK: Should this be the job of the compiler?
+        
+        let currentTouchLocation = trackedTouch.location(in: parent)
         
         #if LOGINPUT
-        let previousTouchLocation = trackedTouch.previousLocation(in: scene)
+        let previousTouchLocation = trackedTouch.previousLocation(in: parent)
         let touchLocationDelta = currentTouchLocation - previousTouchLocation
-        debugLog("trackedTouch.location in scene: \(previousTouchLocation) → \(currentTouchLocation), delta: \(touchLocationDelta), translation: \(String(optional: nodeTouchComponent.touchTranslationInScene))")
+        debugLog("trackedTouch.location in node parent: \(previousTouchLocation) → \(currentTouchLocation), delta: \(touchLocationDelta), translation: \(String(optional: nodeTouchComponent.touchTranslationInParent))")
         #endif
-        
-        // PERFORMANCE: Cache the states so that we don't have to query another class's property too much. CHECK: Should that be the job of the compiler?
         
         let currentTouchState = nodeTouchComponent.state
         let previousTouchState = nodeTouchComponent.previousState
@@ -86,46 +93,47 @@ public final class TouchControlledDraggingComponent: OctopusComponent, OctopusUp
         
         // #3: Store the initial position of the node if the player just began touching it.
         
-        if  initialNodePosition == nil,
+        if  self.initialNodePosition == nil,
             currentTouchState == .touching,
             previousTouchState == .ready // CHECK: for `disabled` too?
         {
-            initialNodePosition = node.position
+            self.initialNodePosition = node.position
         }
         
         // #4.1: Are we tracking a touch?
         
         guard
             let initialNodePosition = self.initialNodePosition,
-            let initialTouchLocationInScene = nodeTouchComponent.initialTouchLocationInScene
+            let initialTouchLocationInParent = nodeTouchComponent.initialTouchLocationInParent
             else { return }
         
         // #4.2: Should we move the node only when tracked touch moves?
         
-        // ℹ️ If the node is only moved when the touch moves, the node may drift away from the touch when the camera or the node's parent moves.
+        // ℹ️ If the node is only moved when the touch moves, the node may drift away from the touch when the camera or the node's parent moves, because this component will not reposition the node to the touch's position.
         
         // PERFORMANCE: If the node's location is always updated every frame regardless of the touch's movement, then the dragging operation will behave as expected, but it may decrease performance.
         
         if onlyMoveWhenTouchTimestampChanges {
             
-            // ⚠️ Do NOT just compare `trackedTouch.location` with `trackedTouch.previousLocation`, because if the touches moves a short distance for one frame then remains stationary, there will always be a difference between `trackedTouch.location` and `trackedTouch.previousLocation`, causing the node to move even if the touch remains stationary. To prevent that, we only move the node if the `trackedTouch.timestamp` has mutated, by checking the `nodeTouchComponent.trackedTouchTimestampDelta`
+            // ℹ️ NOTE: Do NOT compare `trackedTouch.location` with `trackedTouch.previousLocation`, because if the touch moves a short distance for one frame then remains stationary, there will always be a difference between `trackedTouch.location` and `trackedTouch.previousLocation`, causing the node to move even if the touch remains stationary. To prevent that, we only move the node if the `trackedTouch.timestamp` has mutated, by checking the `nodeTouchComponent.trackedTouchTimestampDelta`
             
-            // BUG: 20180502C: APPLEBUG? `UITouch.phase: UITouch.UITouchPhase` does not seem to work for this situation, as it seems to be stuck on `stationary` case throughout the touch.
+            // BUG: 20180502C: APPLEBUG? `UITouch.phase: UITouch.UITouchPhase` does not seem to work for this situation, as it seems to report an `stationary` case by the time this component is updated.
             
             guard nodeTouchComponent.trackedTouchTimestampDelta > 0 else { return }
         }
         
-        // #5: Move the node.
+        // #5: Reposition the node.
         
-        // ⚠️ BUG: 20180502A: APPLEBUG? `UITouch.location(in:)` and `UITouch.previousLocation(in:)` are sometimes not updated for many frames, causing a node to "jump" many pixels after 10 or so frames. Same issue with `preciseLocation(in:)` and `precisePreviousLocation(in:)`
+        // ⚠️ BUG: 20180502A: APPLEBUG? `UITouch.location(in:)` and `UITouch.previousLocation(in:)` are sometimes not updated for many frames, causing a node to remain stationary for 10 or so frames before "jumping" many pixels in one frame. Same issue with `preciseLocation(in:)` and `precisePreviousLocation(in:)`
 
-        // ⚠️ BUG: 20180504B: APPLEBUG? `UITouch.location(in:)` and `UITouch.preciseLocation(in:)` for a touch "wobbles" when a 2nd touch moves near it, even if the tracked touch is stationary. ⚠️ Seems to be a problem on the iOS 11.3 on all devices, in all apps, like Photos.
+        // ⚠️ BUG: 20180504B: APPLEBUG RADAR 39997859: `UITouch.location(in:)` and `UITouch.preciseLocation(in:)` for a touch "wobbles" when a 2nd touch moves near it, even if the tracked touch is stationary. ⚠️ Seems to be a problem since iOS 11.3 on all devices, in all apps, including system apps like Photos.
     
-        // ℹ️ Do not move the node by comparing the `location` and `previousLocation` of the touch. That does not seem to be accurate, and can cause "drifts" where the "pointer" ends up in a different point in the node than where it started touching the node, at least in the iOS Simulator. Instead, store the initial position of the node, then compare the initial position of the touch with its latest position, and directly set the node's position to the final translation. This works the same as the `translation` property of a `UIPanGestureRecognizer`: https://developer.apple.com/documentation/uikit/uipangesturerecognizer/1621207-translation
+        // ℹ️ NOTE: Do not move the node by comparing the `location` and `previousLocation` of the touch. That does not seem to be accurate, and can cause "drifts" where the "pointer" ends up in a different point in the node than where it started touching the node, at least in the iOS Simulator. Instead, store the initial position of the node, then compare the initial position of the touch with its latest position, and directly set the node's position to the final translation. This works the same way as the `translation` property of a `UIPanGestureRecognizer`: https://developer.apple.com/documentation/uikit/uipangesturerecognizer/1621207-translation
         
-        // ℹ️ PERFORMANCE: Calculating the translation here should be faster than accessing the `NodeTouchComponent.touchTranslationInScene` computed property which checks for and unwraps a many optionals (node, scene, etc.)
+        // ℹ️ PERFORMANCE: Calculating the translation here should be faster than accessing the `NodeTouchComponent.touchTranslationInParent` computed property which checks for and unwraps many optionals (node, parent, etc.)
         
-        node.position = initialNodePosition + (currentTouchLocation - initialTouchLocationInScene)
+        node.position = initialNodePosition + (currentTouchLocation - initialTouchLocationInParent)
+        isDragging = true
         
         // #6: Update the interaction state.
         
@@ -133,12 +141,31 @@ public final class TouchControlledDraggingComponent: OctopusComponent, OctopusUp
         
         // ℹ️ When the user performs a dragging operation, a "tap" operation is not expected, so we will instruct the `NodeTouchComponent` to not enter a `tapped` or `endedOutside` state when the user lifts the touch after moving the node.
         
-        // CHECK: Should this suppression of taps be optional? Should we check if the node has moved from its initial position?
+        // CHECK: Should this suppression of taps be optional? Should it depend on whether the node has moved from its initial position?
         
         nodeTouchComponent.updateState(
             suppressStateChangedFlag: false,
             suppressTappedState: true,
             suppressCancelledState: true)
+    }
+    
+    public override func didAddToEntity(withNode node: SKNode) {
+        super.didAddToEntity(withNode: node)
+        
+        // A scene itself is not really draggable, so...
+        
+        if node is SKScene {
+            OctopusKit.logForWarnings.add("A TouchControlledDraggingComponent cannot be added to the scene entity — Removing.")
+            OctopusKit.logForTips.add("See CameraPanComponent.")
+            self.removeFromEntity()
+        }
+    }
+    
+    public override func willRemoveFromEntity() {
+        super.willRemoveFromEntity()
+        
+        self.initialNodePosition = nil
+        self.isDragging = false
     }
 }
 
