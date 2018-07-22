2f5128a9838dd92c8301485be969c9c22098758b
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2018-07-22 23:43:22 +0500

    Add TouchControlledPhysicsHoldingComponent

---
+++ Sources/OctopusKit/Components/Input/Touch/TouchControlledPhysicsHoldingComponent.swift
@@ -0,0 +1,78 @@
+//
+//  TouchControlledPhysicsHoldingComponent.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2018/07/07.
+//  Copyright © 2018 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import SpriteKit
+import GameplayKit
+
+/// Prevents the physics body of the entity's `SpriteKitComponent` node from moving while the node is touched by the player.
+///
+/// - Prevents the body from being affected by gravity while it is being held.
+/// - Sets the body's velocity to `0` every frame while it is being held.
+/// - Optionally sets the body's angular velocity to `0` while it is being held, but the body may still be rotated by other forces (such as contact with other bodies.)
+/// - These properties are reapplied every frame.
+///
+/// **Dependencies:** `NodeTouchComponent`, `PhysicsComponent`
+final class TouchControlledPhysicsHoldingComponent: OctopusComponent, OctopusUpdatableComponent {
+    
+    override var requiredComponents: [GKComponent.Type]? {
+        return [PhysicsComponent.self,
+                NodeTouchComponent.self]
+    }
+    
+    /// If `true`, the body's angular velocity is set to `0` in every frame while the node is being held, but the body may still be rotated by other forces (such as contact with other bodies.)
+    public var stopAngularVelocity: Bool = false
+    
+    public var isHolding: Bool = false {
+        didSet {
+            #if LOGINPUT
+            if isHolding != oldValue { debugLog("= \(oldValue) → \(isHolding)") }
+            #endif
+        }
+    }
+    
+    public override func didAddToEntity(withNode node: SKNode) {
+        super.didAddToEntity(withNode: node)
+        
+        // This component does not make sense on a scene, so...
+        
+        if node is SKScene {
+            OctopusKit.logForWarnings.add("A TouchControlledPhysicsHoldingComponent cannot be added to the scene entity — Removing.")
+            self.removeFromEntity()
+        }
+    }
+    
+    override func update(deltaTime seconds: TimeInterval) {
+
+        let nodeTouchComponent = coComponent(ofType: NodeTouchComponent.self)
+        
+        let isBeingTouched = (nodeTouchComponent?.trackedTouch != nil)
+        
+        isBeingTouched ? holdBody() : releaseBody()
+    }
+    
+    private func holdBody() {
+        guard let physicsBody = coComponent(ofType: PhysicsComponent.self)?.physicsBody else { return }
+        physicsBody.velocity = CGVector.zero
+        physicsBody.affectedByGravity = false
+        if stopAngularVelocity { physicsBody.angularVelocity = 0 }
+        isHolding = true
+    }
+    
+    override func willRemoveFromEntity() {
+        super.willRemoveFromEntity()
+        releaseBody()
+        isHolding = false
+    }
+    
+    private func releaseBody() {
+        guard let physicsBody = coComponent(ofType: PhysicsComponent.self)?.physicsBody else { return }
+        physicsBody.affectedByGravity = true
+        isHolding = false
+    }
+}
+
