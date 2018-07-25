1eca5f43a33f3a8ca58081ffd25bdf1d76320a38
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2018-07-25 05:21:47 +0500

    Update MotionControlledThrustComponent
    - Improve comments.

--- Sources/OctopusKit/Components/Input/Motion/MotionControlledThrustComponent.swift
+++ Sources/OctopusKit/Components/Input/Motion/MotionControlledThrustComponent.swift
@@ -5,39 +5,35 @@
 //  Created by ShinryakuTako@invadingoctopus.io on 2017/12/06.
 //  Copyright © 2018 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
 //
+
 // TODO: Other types of motion input sources (e.g. besides deviceMotion)
-//
 
 import GameplayKit
 
 #if os(iOS)
     
-/// Controls the owning entity's `ThrustComponent` based on the motion data from the supplied `MotionManagerComponent` (which may be provided via an `InputRelayComponent`).
+/// Controls the entity's `ThrustComponent` based on the motion data from a `MotionManagerComponent`.
 ///
 /// **Dependencies:** `MotionManagerComponent`, `ThrustComponent`
 public final class MotionControlledThrustComponent: OctopusComponent, OctopusUpdatableComponent {
     
     public override var requiredComponents: [GKComponent.Type]? {
-        return [ThrustComponent.self,
-                MotionManagerComponent.self]
+        return [MotionManagerComponent.self,
+                ThrustComponent.self]
     }
     
     public override func update(deltaTime seconds: TimeInterval) {
-        super.update(deltaTime: seconds)
-        
         guard let motionManagerComponent = coComponent(MotionManagerComponent.self) else { return }
         
-        // Tilt to move/thrust
+        // Tilt to move/thrust.
         
-        if
-            let motion = motionManagerComponent.motionManager?.deviceMotion,
+        if  let motion = motionManagerComponent.motionManager?.deviceMotion,
             let thrustComponent = coComponent(ThrustComponent.self)
         {
             thrustComponent.thrustVector = CGVector(dx: CGFloat(motion.gravity.x),
                                                     dy: CGFloat(motion.gravity.y))
         }
     }
-    
 }
 
 #else
