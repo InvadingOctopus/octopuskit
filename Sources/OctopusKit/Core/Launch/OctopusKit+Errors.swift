3bfc03398d6c20e42b4109caa41a26a956e3f774
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2018-08-30 10:09:06 +0500

    Add OctopusKit.GenericError
    - For generic error messages; may be replaced with something better.

---
+++ Sources/OctopusKit/Core/Launch/OctopusKit+Errors.swift
@@ -0,0 +1,17 @@
+//
+//  OctopusKit+Errors.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2018/08/30.
+//  Copyright © 2018 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import Foundation
+
+public extension OctopusKit {
+    
+    public struct GenericError: Error {
+        let message: String
+    }
+    
+}
