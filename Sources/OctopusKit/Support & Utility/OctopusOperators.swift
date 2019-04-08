74d2e9019878c2828aa36abefaf626f97f8b0cd6
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-04-09 02:41:53 +0400

    Add ∈ operator

---
+++ Sources/OctopusKit/Support & Utility/OctopusOperators.swift
@@ -0,0 +1,18 @@
+//
+//  OctopusOperators.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2018/12/19.
+//  Copyright © 2018 Invading Octopus. All rights reserved.
+//
+
+import Foundation
+
+infix operator ∈
+
+/// Returns `true` if the right-hand array contains the left-hand value.
+public func ∈ <T: Equatable> (left: T, right: [T]) -> Bool {
+    // CREDIT: https://gist.github.com/AliSoftware
+    // CREDIT: https://gist.github.com/JohnSundell/1956ce36b9303eb4bf912da0de9e2844
+    return right.contains(left)
+}
