1a15e6d331b82bbd5a37c7e9e016350b6a8f2641
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-10-05 19:10:13 +0500

    Improve identation

--- Sources/OctopusKit/Components/Graphics/OctopusSprite/OctopusSprite+Input-iOS.swift
+++ Sources/OctopusKit/Components/Graphics/OctopusSprite/OctopusSprite+Input-iOS.swift
@@ -10,49 +10,49 @@ import SpriteKit
 import GameplayKit
 
 #if os(iOS)
-    
+
 extension OctopusSprite: TouchEventComponentCompatible {
+    
+    /// `super` must be called when overriding, to ensure proper operation.
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        /// `super` must be called when overriding, to ensure proper operation.
-        public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-            if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
-                inputComponent.touchesBegan = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
-            }
+        if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
+            inputComponent.touchesBegan = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
         }
-        
-        /// `super` must be called when overriding, to ensure proper operation.
-        public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-            if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
-                inputComponent.touchesMoved = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
-            }
-        }
-        
-        /// `super` must be called when overriding, to ensure proper operation.
-        public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-            if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
-                inputComponent.touchesCancelled = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
-            }
-        }
-        
-        /// `super` must be called when overriding, to ensure proper operation.
-        public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-            if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
-                inputComponent.touchesEnded = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
-            }
-        }
-        
-        /// `super` must be called when overriding, to ensure proper operation.
-        public override func touchesEstimatedPropertiesUpdated(_ touches: Set<UITouch>) {
-            
-            if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
-                inputComponent.touchesEstimatedPropertiesUpdated = TouchEventComponent.TouchEvent(touches: touches, event: nil, node: self)
-            }
-        }
-        
     }
     
+    /// `super` must be called when overriding, to ensure proper operation.
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
+            inputComponent.touchesMoved = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
+        }
+    }
+    
+    /// `super` must be called when overriding, to ensure proper operation.
+    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
+            inputComponent.touchesCancelled = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
+        }
+    }
+    
+    /// `super` must be called when overriding, to ensure proper operation.
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
+            inputComponent.touchesEnded = TouchEventComponent.TouchEvent(touches: touches, event: event, node: self)
+        }
+    }
+    
+    /// `super` must be called when overriding, to ensure proper operation.
+    public override func touchesEstimatedPropertiesUpdated(_ touches: Set<UITouch>) {
+        
+        if let inputComponent = self.entity?.componentOrRelay(ofType: TouchEventComponent.self) {
+            inputComponent.touchesEstimatedPropertiesUpdated = TouchEventComponent.TouchEvent(touches: touches, event: nil, node: self)
+        }
+    }
+    
+}
+
 #endif
