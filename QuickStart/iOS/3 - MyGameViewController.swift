3976815b2bf552ae6a44de9577a0e88ec9c267ab
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-10-20 18:11:12 +0500

    Update documentation
    - Add explanations for the new object and view hierarchy
    - Move MyGameViewController down from Step 2 to Step 3

--- QuickStart/iOS/3 - MyGameViewController.swift
+++ QuickStart/iOS/3 - MyGameViewController.swift
@@ -1,18 +1,23 @@
 //
-//  GameViewController.swift
+//  MyGameViewController.swift
 //  OctopusKitQuickStart
 //
 //  Created by ShinryakuTako@invadingoctopus.io on 2018/06/04.
 //  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
 //
 
-//  🔶 STEP 2: The view controller for the SpriteKit view (SKView) that displays your game.
+//  🔶 STEP 3: The view controller for the SpriteKit view (SKView) that displays your game.
 //
-//  Creating a subclass of OctopusViewController is not necessary for an OctopusKit, but complex games may require some view controller customization.
+//  Creating a subclass of OctopusViewController is not necessary for a basic OctopusKit project, but complex games may require some view controller customization. This subclass does no customization but is provided for illustration.
 //
-//  If you are using SwiftUI, then simply create an OctopusKitView:
+//  In SwiftUI, an OctopusViewController is encapsulated by OctopusViewControllerRepresentable.
 //
-//  OctopusKitView<MyGameViewController>(gameCoordinatorOverride: QuickStartGameCoordinator())
+//  However, to use OctopusKit in a SwiftUI project, use:
+//
+//  OctopusKitContainerView<MyGameCoordinator, MyGameViewController>()
+//      .environmentObject(MyGameCoordinator())
+//
+//  The OctopusKitContainerView presents SpriteKit and SwiftUI content together.
 //
 //  If you are using AppKit or UIKit, then the view controller for the SKView in your main storyboard must be the OctopusViewController class, or a subclass of it that is specific to your game, like the MyGameViewController in this project.
 
@@ -22,7 +27,7 @@ final class MyGameViewController: OctopusViewController {
     
     override func viewDidLoad() {
         
-        // 🔶 STEP 2.1: You may customize some screen-related settings here, such as the device orientations allowed in your game and status bar visibility etc.
+        // 🔶 STEP 3.1: You may customize some screen-related settings here, such as the device orientations allowed in your game and status bar visibility etc.
         
         super.viewDidLoad() // ❗️ Required. You must call super.viewDidLoad() before any other code in your overriding implementation.
         
