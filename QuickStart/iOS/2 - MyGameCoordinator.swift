39b1ba9293b2e4595b849d047b098ad7530c91a3
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-10-20 17:44:29 +0500

    Rename QuickStartGameController to MyGameCoordinator
    - Update documentation

---
+++ QuickStart/iOS/2 - MyGameCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  MyGameCoordinator.swift
+//  OctopusKitQuickStart
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2018/02/10.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+//  🔶 STEP 2: The OctopusGameCoordinator is the master coordinator for an OctopusKit game.
+//
+//  It lists all the valid states for your game and specifies the initial state.
+//
+//  In the MVC hierarchy, it's a "controller" (as in "ViewController" etc.)
+//
+//  You may also use the game coordinator to store objects or properties that must be shared across all states and scenes, such as the game world, player data and network connections etc.
+//
+//  Each OctopusGameState has a scene and UI overlay associated with it; scenes present the content for each state.
+//
+//  Creating a subclass of OctopusGameCoordinator is not necessary for basic OctopusKit projects so you may simply use OctopusGameCoordinator(states:initialStateClass:)
+//
+//  Complex games may require custom coordinators to manage different kinds of global state and external connections.
+
+import OctopusKit
+
+final class MyGameCoordinator: OctopusGameCoordinator {
+    
+    init() {
+        super.init(states: [LogoState(),
+                            TitleState(),
+                            PlayState(),
+                            PausedState(),
+                            GameOverState()],
+                   initialStateClass: LogoState.self)
+    }
+    
+}
+
