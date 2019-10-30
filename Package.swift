86594890bc1d3b7a48b36a058e92c24b66813d08
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-10-30 14:21:23 +0500

    Add macOS and tvOS platforms to package manifest
    For now, OK may not work on macOS or tvOS projects out of the box without some manual modifications.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "OctopusKit",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
