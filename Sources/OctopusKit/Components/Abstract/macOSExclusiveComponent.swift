abb0d10cb1619a0e7c1cef3911588faf67b5efbd
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-03 01:28:37 +0500

    Create macOSExclusiveComponent.swift

---
+++ Sources/OctopusKit/Components/Abstract/macOSExclusiveComponent.swift
@@ -0,0 +1,30 @@
+//
+//  macOSExclusiveComponent.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/11/2.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import GameplayKit
+
+#if os(macOS)
+
+/// A "dummy" base class for components that are not compatible with iOS.
+public typealias macOSExclusiveComponent = OctopusComponent
+    
+#else
+
+/// A "dummy" base class for components that are not compatible with iOS.
+open class macOSExclusiveComponent: OctopusComponent {
+    
+    public override init() {
+        // TO DECIDE: Error or warning?
+        OctopusKit.logForErrors.add("\(type(of: self)) is for macOS only!")
+        super.init()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) { fatalError("\(Self.self)) is for macOS only!") }
+}
+
+#endif
