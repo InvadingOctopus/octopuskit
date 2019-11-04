f02e72ccad4b530ebb20baa309a39d961155e49e
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-04 07:10:55 +0500

    Create LogChanges.swift
    - Create Property Wrappers folder

---
+++ Sources/OctopusKit/Support & Utility/Property Wrappers/LogChanges.swift
@@ -0,0 +1,26 @@
+//
+//  LogChanges.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/11/4.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import Foundation
+
+/// A wrapper which prints a `debugLog` of any changes to the wrapped property.
+@propertyWrapper
+public struct LogChanges <ValueType: Equatable> {
+    
+    public var wrappedValue: ValueType {
+        didSet {
+            if wrappedValue != oldValue {
+                debugLog("= \(String(optional: wrappedValue))")
+            }
+        }
+    }
+    
+    public init(wrappedValue: ValueType) {
+        self.wrappedValue = wrappedValue
+    }
+}
