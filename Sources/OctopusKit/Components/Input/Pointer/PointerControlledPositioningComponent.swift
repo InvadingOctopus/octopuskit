cc6c50514ec3eb4b3885fe5b1f0ce3a6103498b2
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-14 22:36:27 +0500

    Create PointerControlledPositioningComponent.swift

---
+++ Sources/OctopusKit/Components/Input/Pointer/PointerControlledPositioningComponent.swift
@@ -0,0 +1,45 @@
+//
+//  PointerControlledPositioningComponent.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/11/14.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import SpriteKit
+import GameplayKit
+
+/// Sets the position of the entity's `SpriteKitComponent` node to the location of the pointer received by the entity's `PointerEventComponent`.
+///
+/// **Dependencies:** `SpriteKitComponent`, `PointerEventComponent`
+public final class PointerControlledPositioningComponent: OctopusComponent, OctopusUpdatableComponent {
+    
+    public override var requiredComponents: [GKComponent.Type]? {
+        [SpriteKitComponent.self,
+         PointerEventComponent.self]
+    }
+    
+    // These `init`s are for Xcode Scene Editor support.
+    public override init() { super.init() }
+    public required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) }
+    
+    public override func update(deltaTime seconds: TimeInterval) {
+        
+        guard
+            let node    = self.entityNode,
+            let parent  = node.parent,
+            let pointerEventComponent = coComponent(PointerEventComponent.self),
+            let latestEvent = pointerEventComponent.latestEventForCurrentFrame
+            else { return }
+        
+        node.position = latestEvent.location(in: parent)
+        
+        // Update the state of a `NodePointerStateComponent`, if present, for the new position.
+        
+        if  let nodePointerComponent = coComponent(NodePointerStateComponent.self) {
+            nodePointerComponent.updateState(suppressTappedState:    true,
+                                             suppressCancelledState: true)
+        }
+        
+    }
+}
