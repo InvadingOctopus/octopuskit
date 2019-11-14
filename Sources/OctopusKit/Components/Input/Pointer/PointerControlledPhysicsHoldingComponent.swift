e9dfba0dbc715eb43e9274e38e2e349a2bf827ac
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-14 22:42:54 +0500

    Create PointerControlledPhysicsHoldingComponent.swift

---
+++ Sources/OctopusKit/Components/Input/Pointer/PointerControlledPhysicsHoldingComponent.swift
@@ -0,0 +1,74 @@
+//
+//  PointerControlledPhysicsHoldingComponent.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/11/14.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import SpriteKit
+import GameplayKit
+
+/// Prevents the physics body of the entity's `SpriteKitComponent` node from moving while the node is touched or clicked by the player.
+///
+/// - Prevents the body from being affected by gravity while it is being held.
+/// - Sets the body's velocity to `0` every frame while it is being held.
+/// - Optionally sets the body's angular velocity to `0` while it is being held, but the body may still be rotated by other forces (such as contact with other bodies.)
+/// - These properties are reapplied every frame.
+///
+/// **Dependencies:** `NodePointerStateComponent`, `PhysicsComponent`
+public final class PointerControlledPhysicsHoldingComponent: OctopusComponent, OctopusUpdatableComponent {
+    
+    public override var requiredComponents: [GKComponent.Type]? {
+        return [PhysicsComponent.self,
+                NodePointerStateComponent.self]
+    }
+    
+    /// If `true`, the body's angular velocity is set to `0` in every frame while the node is being held, but the body may still be rotated by other forces (such as contact with other bodies.)
+    ///
+    /// Defaults to `false`.
+    public var stopAngularVelocity: Bool = false
+    
+    @LogInputEventChanges(propertyName: "PointerControlledPhysicsHoldingComponent.isHolding")
+    public var isHolding: Bool = false
+    
+    public override func didAddToEntity(withNode node: SKNode) {
+        super.didAddToEntity(withNode: node)
+        
+        // This component does not make sense on a scene, so...
+        
+        if  node is SKScene {
+            OctopusKit.logForWarnings.add("A PointerControlledPhysicsHoldingComponent cannot be added to the scene entity — Removing.")
+            self.removeFromEntity()
+        }
+    }
+    
+    public override func update(deltaTime seconds: TimeInterval) {
+
+        let nodePointerComponent = coComponent(ofType: NodePointerStateComponent.self)
+        
+        let isNodeBeingPointed = (nodePointerComponent?.state != nil && nodePointerComponent!.state == .pointing)
+        
+        isNodeBeingPointed ? holdBody() : releaseBody()
+    }
+    
+    private func holdBody() {
+        guard let physicsBody = coComponent(ofType: PhysicsComponent.self)?.physicsBody else { return }
+        physicsBody.velocity = CGVector.zero
+        physicsBody.affectedByGravity = false
+        if stopAngularVelocity { physicsBody.angularVelocity = 0 }
+        isHolding = true
+    }
+    
+    public override func willRemoveFromEntity() {
+        super.willRemoveFromEntity()
+        releaseBody()
+        isHolding = false
+    }
+    
+    private func releaseBody() {
+        guard let physicsBody = coComponent(ofType: PhysicsComponent.self)?.physicsBody else { return }
+        physicsBody.affectedByGravity = true
+        isHolding = false
+    }
+}
