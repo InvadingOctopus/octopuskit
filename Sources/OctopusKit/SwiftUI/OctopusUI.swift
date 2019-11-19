8fba3f8a4dc35a596aa2f9e1ce044d5d01670a33
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-20 01:20:27 +0500

    Add a link to OctopusUI

---
+++ Sources/OctopusKit/SwiftUI/OctopusUI.swift
@@ -0,0 +1,9 @@
+//
+//  OctopusUI.swift
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/11/20
+//
+
+//  For a library of open-source SwiftUI controls and extensions, please the OctopusUI repository:
+//
+//  https://github.com/InvadingOctopus/octopusui
