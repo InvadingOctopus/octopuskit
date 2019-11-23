e82f9a10225163158fe24f7bd53fadca13ee0a1f
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-23 16:14:15 +0500

    Fix KeyboardControlledForceComponent
    - We have to use per-second values, not per-frame, silly!
    - Change reversal flags to multiplication factors which allow scaling or disabling an axis.
    - Improve logging
    - Improve documentation

--- Sources/OctopusKit/Components/Input/Keyboard/KeyboardControlledForceComponent.swift
+++ Sources/OctopusKit/Components/Input/Keyboard/KeyboardControlledForceComponent.swift
@@ -13,25 +13,16 @@ import GameplayKit
 
 /// Applies a force to the entity's `PhysicsComponent` body on every frame, based on `KeyboardEventComponent` input.
 ///
-/// **Dependencies:** `KeyboardEventComponent`, `PhysicsComponent`
+/// Set the `LOGINPUTEVENTS` compilation flag to log values.
 ///
-/// Set the `LOGINPUTEVENTS` compilation flag to log the `acceleratedMagnitude`.
+/// **Dependencies:** `KeyboardEventComponent`, `PhysicsComponent`
 @available(macOS 10.15, *)
 public final class KeyboardControlledForceComponent: OctopusComponent, OctopusUpdatableComponent {
     
     public override var requiredComponents: [GKComponent.Type]? {
-        [PhysicsComponent.self,
-         KeyboardEventComponent.self]
+        [KeyboardEventComponent.self,
+        PhysicsComponent.self]
     }
-    
-    public var baseMagnitude:           CGFloat
-    public var maximumMagnitude:        CGFloat
-    public var acceleratedMagnitude:    CGFloat = 0
-    
-    /// The amount to increase `acceleratedMagnitude` by on every frame. `acceleratedMagnitude` is reset to the base value when there is no keyboard input.
-    public var accelerationPerFrame:    CGFloat
-    
-    public var reverseHorizontal, reverseVertical: Bool
     
     /// Change this to a different code to customize the keys.
     public var arrowUp:     UInt16 = .arrowUp
@@ -42,24 +33,33 @@ public final class KeyboardControlledForceComponent: OctopusComponent, OctopusUp
     /// Change this to a different code to customize the keys.
     public var arrowLeft:   UInt16 = .arrowLeft
     
+    public var baseMagnitudePerSecond:      CGFloat
+    public var maximumMagnitudePerSecond:   CGFloat
+    public var acceleratedMagnitude:        CGFloat = 0
+    
+    /// The amount to increase `acceleratedMagnitude` by per second, while there is keyboard input. `acceleratedMagnitude` is reset to `baseMagnitudePerSecond` when there is no keyboard input.
+    public var accelerationPerSecond:       CGFloat
+    
+    public var horizontalFactor:            CGFloat = 1
+    public var verticalFactor:              CGFloat = 1
     
     /// - Parameters:
-    ///   - baseMagnitude: The minimum magnitude to apply to the physics body on every frame.
-    ///   - maximumMagnitude: The maximum magnitude to allow after acceleration has been applied.
-    ///   - accelerationPerFrame: The amount to increase the magnitude by on every frame while there is keyboard input. The magnitude is reset to the `baseMagnitude` when there is no keyboard input.
-    ///   - reverseHorizontal: Reverse the X axis.
-    ///   - reverseVertical: Reverse the Y axis.
-    public init(baseMagnitude:          CGFloat = 10,
-                maximumMagnitude:       CGFloat = 10,
-                accelerationPerFrame:   CGFloat = 0,
-                reverseHorizontal:      Bool    = false,
-                reverseVertical:        Bool    = false)
+    ///   - baseMagnitudePerSecond: The minimum magnitude to apply to the physics body every second.
+    ///   - maximumMagnitudePerSecond: The maximum magnitude to allow after acceleration has been applied.
+    ///   - accelerationPerSecond: The amount to increase the magnitude by per second, while there is keyboard input. The magnitude is reset to the `baseMagnitudePerSecond` when there is no keyboard input.
+    ///   - horizontalFactor: Multiply the X axis force by this factor. Default: `1`. To reverse the X axis, specify a negative value like `-1`. To disable the X axis, specify `0`.
+    ///   - verticalFactor: Multiply the Y axis force by this factor. Default: `1`. To reverse the Y axis, specify a negative value like `-1`. To disable the Y axis, specify `0`.
+    public init(baseMagnitudePerSecond:     CGFloat = 600,  // ÷ 60 = 10 per frame
+                maximumMagnitudePerSecond:  CGFloat = 1200, // ÷ 60 = 20 per frame
+                accelerationPerSecond:      CGFloat = 600,
+                horizontalFactor:           CGFloat = 1,
+                verticalFactor:             CGFloat = 1)
     {
-        self.baseMagnitude          = baseMagnitude
-        self.maximumMagnitude       = maximumMagnitude
-        self.accelerationPerFrame   = accelerationPerFrame
-        self.reverseHorizontal      = reverseHorizontal
-        self.reverseVertical        = reverseVertical
+        self.baseMagnitudePerSecond     = baseMagnitudePerSecond
+        self.maximumMagnitudePerSecond  = maximumMagnitudePerSecond
+        self.accelerationPerSecond      = accelerationPerSecond
+        self.horizontalFactor           = horizontalFactor
+        self.verticalFactor             = verticalFactor
         super.init()
     }
     
@@ -72,43 +72,42 @@ public final class KeyboardControlledForceComponent: OctopusComponent, OctopusUp
             let keyboardEventComponent = coComponent(KeyboardEventComponent.self),
             !keyboardEventComponent.codesPressed.isEmpty,
             let physicsBody = coComponent(PhysicsComponent.self)?.physicsBody
-            else
-        {
-            acceleratedMagnitude = baseMagnitude
-            return
+            else {
+                acceleratedMagnitude = baseMagnitudePerSecond // TODO: PERFORMANCE: Figure out a better way than setting this every frame.
+                return
         }
         
-        #if LOGINPUTEVENTS
-        debugLog("acceleratedMagnitude: \(acceleratedMagnitude)")
-        #endif
-        
         // Did player press a directional arrow key?
-        
-        // ❕ NOTE: Don't use `switch` or `else` because we want to process multiple keypresses, to generate diagonal forces and also cancel out opposing arrow keys.
+        // ❕ NOTE: Don't use `switch` or `else` because we want to process multiple keypresses, to generate diagonal forces and also cancel out opposing directions.
         
         let codesPressed = keyboardEventComponent.codesPressed
-        var vector       = CGVector.zero
+        let magnitudeForCurrentFrame = acceleratedMagnitude * CGFloat(seconds)
+        var vector = CGVector.zero
+
+        if codesPressed.contains(self.arrowUp)    { vector.dy += magnitudeForCurrentFrame } // ⬆️
+        if codesPressed.contains(self.arrowRight) { vector.dx += magnitudeForCurrentFrame } // ➡️
+        if codesPressed.contains(self.arrowDown)  { vector.dy -= magnitudeForCurrentFrame } // ⬇️
+        if codesPressed.contains(self.arrowLeft)  { vector.dx -= magnitudeForCurrentFrame } // ⬅️
         
-        if codesPressed.contains(arrowUp)    { vector.dy += acceleratedMagnitude } // ⬆️
-        if codesPressed.contains(arrowRight) { vector.dx += acceleratedMagnitude } // ➡️
-        if codesPressed.contains(arrowDown)  { vector.dy -= acceleratedMagnitude } // ⬇️
-        if codesPressed.contains(arrowLeft)  { vector.dx -= acceleratedMagnitude } // ⬅️
+        // Apply any multipliers.
         
-        // Apply any optional inversions.
-        
-        if reverseHorizontal { vector.dx = -vector.dx }
-        if reverseVertical { vector.dy = -vector.dy }
+        vector.dx *= horizontalFactor
+        vector.dy *= verticalFactor
         
         // Apply the final vector to the body.
-                
+        
+        #if LOGINPUTEVENTS
+        debugLog("acceleratedMagnitude: \(acceleratedMagnitude), magnitudeForCurrentFrame: \(magnitudeForCurrentFrame), horizontalFactor: \(horizontalFactor), verticalFactor: \(horizontalFactor), force: \(vector)")
+        #endif
+        
         physicsBody.applyForce(vector)
         
         // Apply acceleration for the next frame.
         
-        if  acceleratedMagnitude < maximumMagnitude {
-            acceleratedMagnitude += accelerationPerFrame
-            if  acceleratedMagnitude > maximumMagnitude {
-                acceleratedMagnitude = maximumMagnitude
+        if  acceleratedMagnitude < maximumMagnitudePerSecond {
+            acceleratedMagnitude += (accelerationPerSecond * CGFloat(seconds))
+            if  acceleratedMagnitude > maximumMagnitudePerSecond {
+                acceleratedMagnitude = maximumMagnitudePerSecond
             }
         }
     }
@@ -119,5 +118,6 @@ public final class KeyboardControlledForceComponent: OctopusComponent, OctopusUp
 #if !canImport(AppKit)
 // TODO: Add support for iOS/tvOS keyboards.
 @available(iOS, unavailable)
+@available(tvOS, unavailable)
 public final class KeyboardControlledForceComponent: macOSExclusiveComponent {}
 #endif
