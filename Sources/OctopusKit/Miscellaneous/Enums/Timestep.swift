1a69c81b4429afd2c88b468492274d99f8e35521
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-29 12:41:54 +0400

    Create Timestep.swift
    For specifying a fixed per-frame or variable per-second timestep in time-dependent components.

---
+++ Sources/OctopusKit/Miscellaneous/Enums/Timestep.swift
@@ -0,0 +1,25 @@
+//
+//  Timestep.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/11/28.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import Foundation
+
+/// Specifies the timestep for time-dependent components.
+public enum Timestep {
+    
+    /// Fixed timestep; applies a constant `…perUpdate` change to the affected values in `update(deltaTime:)` every frame.
+    ///
+    /// Use this when slower gameplay is preferred to losing frames.
+    case perFrame
+    
+    /// Variable timestep; Multiples each `…perUpdate` change by `deltaTime` in `update(deltaTime:)` every frame.
+    ///
+    /// Use this when losing frames is preferred to slower gameplay.
+    case perSecond
+}
+
+// Prompted by a discussion on the Reddit /r/GameDev Discord. :)
