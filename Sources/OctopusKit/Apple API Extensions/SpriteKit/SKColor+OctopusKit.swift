6848d7b6a32e66b956d775d92a886dda25123d87
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-29 16:59:35 +0400

    Update SKColor+OctopusKit.swift
    - Add P3 and SRGB variants
    - Add systemPrimaryColors
    - Add random(in:)
    - Add @inlinable
    - Reduce code duplication

--- Sources/OctopusKit/Apple API Extensions/SpriteKit/SKColor+OctopusKit.swift
+++ Sources/OctopusKit/Apple API Extensions/SpriteKit/SKColor+OctopusKit.swift
@@ -13,32 +13,96 @@ import SpriteKit
 
 extension SKColor {
     
-    // Extend `SKColor` so it applies to both `UIColor` and `CGColor`.
+    // NOTE: `SKColor` is an OS-dependent typealias for either `NSColor` or `UIColor`.
+    
+    // CHECK: Does `SpriteKit` correctly support Display-P3?
     
     /// Shorthand for `withAlphaComponent(_:)`
     ///
     /// Useful for chaining calls to `SKColor` initializers.
     open func withAlpha(_ alpha: CGFloat) -> SKColor {
-        return self.withAlphaComponent(alpha)
+        self.withAlphaComponent(alpha)
+    }
+    
+    /// Returns a random color in the Display-P3 gamut with RGB values within the specified range, where the maximum saturation is `1.0`.
+    @inlinable
+    public static func randomP3(in range: ClosedRange<CGFloat>) -> SKColor {
+        // TODO: Add rounding parameter
+        // TODO: Add generator parameter
+        let red   = CGFloat.random(in: range)
+        let green = CGFloat.random(in: range)
+        let blue  = CGFloat.random(in: range)
+        return SKColor(displayP3Red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    /// Returns a random color in the SRGB gamut with RGB values within the specified range, where the maximum saturation is `1.0`.
+    @inlinable
+    public static func randomSRGB(in range: ClosedRange<CGFloat>) -> SKColor {
+        // TODO: Add rounding parameter
+        // TODO: Add generator parameter
+        let red   = CGFloat.random(in: range)
+        let green = CGFloat.random(in: range)
+        let blue  = CGFloat.random(in: range)
+        return SKColor(srgbRed: red, green: green, blue: blue, alpha: 1.0)
     }
     
     // MARK: - Custom Color Collections
     
-    /// A collection consisting of red, green and blue.
+    /// An array consisting of red, green and blue.
+    @inlinable
     public static var primaryColors: [SKColor] {
-        return [.red, .green, .blue]
+        [.red, .green, .blue]
     }
     
-    /// A collection of bright colors in the Display-P3 gamut.
+    /// An array consisting of the *system-dependent* red, green and blue.
+    @inlinable
+    public static var systemPrimaryColors: [SKColor] {
+        [.systemRed, .systemGreen, .systemBlue]
+    }
+    
+    /// Each combination of `(1.0, 0.75, 0.5)` and `(1.0, 1.0, 0.25)`.
+    @inlinable
+    public static var brightRGBValues: [(red: CGFloat, green: CGFloat, blue: CGFloat)] {
+        [(1.0,  0.75, 0.5),
+         (1.0,  0.5,  0.75),
+         
+         (0.75, 1.0,  0.5),
+         (0.5,  1.0,  0.75),
+         
+         (0.75, 0.5,  1.0),
+         (0.5,  0.75, 1.0),
+         
+         (1.0,  1.0,  0.25),
+         (0.25, 1.0,  1.0),
+         (1.0,  0.25, 1.0)]
+    }
+    
+    /// An array of bright colors.
+    ///
+    /// Uses the `brightRGBValues` property.
+    @inlinable
     public static var brightColors: [SKColor] {
-        // CHECK: Does `SpriteKit` correctly support Display-P3?
-        return [SKColor(displayP3Red: 1.0, green: 0.75, blue: 0.5, alpha: 1.0),
-                SKColor(displayP3Red: 0.5, green: 1.0, blue: 0.75, alpha: 1.0),
-                SKColor(displayP3Red: 0.75, green: 0.5, blue: 1.0, alpha: 1.0),
-                SKColor(displayP3Red: 1.0, green: 1.0, blue: 0.25, alpha: 1.0),
-                SKColor(displayP3Red: 0.25, green: 1.0, blue: 1.0, alpha: 1.0),
-                SKColor(displayP3Red: 1.0, green: 0.25, blue: 1.0, alpha: 1.0)
-        ]
+        // TODO: Check and return the system-dependent gamut.
+        brightColorsP3
     }
     
+    /// An array of bright colors in the Display-P3 gamut.
+    ///
+    /// Uses the `brightRGBValues` property.
+    @inlinable
+    public static var brightColorsP3: [SKColor] {
+        brightRGBValues.map {
+            SKColor(displayP3Red: $0.red, green: $0.green, blue: $0.blue, alpha: 1.0)
+        }
+    }
+    
+    /// An array of bright colors in the sRGB gamut.
+    ///
+    /// Uses the `brightRGBValues` property.
+    @inlinable
+    public static var brightColorsSRGB: [SKColor] {
+        brightRGBValues.map {
+            SKColor(srgbRed: $0.red, green: $0.green, blue: $0.blue, alpha: 1.0)
+        }
+    }
 }
