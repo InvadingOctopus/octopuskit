47f512ed9a91305fdd73458136bde37289f330e5
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-11-29 16:30:22 +0400

    Add TimeStep.apply(_:to:deltaTime:)

--- Sources/OctopusKit/Miscellaneous/Enums/TimeStep.swift
+++ Sources/OctopusKit/Miscellaneous/Enums/TimeStep.swift
@@ -20,6 +20,21 @@ public enum TimeStep {
     ///
     /// Use this when losing frames is preferred to slower gameplay.
     case perSecond
+    
+    /// Shorthand for `value += (timestep == .perFrame) ? change : change * deltaTime`
+    @inlinable
+    public func apply <Number> (_ change:  Number,
+                                to value:  inout Number,
+                                deltaTime: Number)
+        where Number: Numeric
+    {
+        value += (self == .perFrame) ? change : change * deltaTime
+//        switch self {
+//        case .perFrame:     value += change
+//        case .perSecond:    value += change * deltaTime
+//        @unknown default:   fatalError("\(self) case not supported.")
+//        }
+    }
 }
 
 // Prompted by a discussion on the Reddit /r/GameDev Discord. :)
