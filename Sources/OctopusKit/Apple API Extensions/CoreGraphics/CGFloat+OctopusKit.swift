791a0cba839f9a90a493999aee57fa15b58945fb
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-12-12 04:34:21 +0400

    Update CGFloat+OctopusKit.swift
    - Move randomInteger type methods to a more general extension on FloatingPoint

--- Sources/OctopusKit/Apple API Extensions/CoreGraphics/CGFloat+OctopusKit.swift
+++ Sources/OctopusKit/Apple API Extensions/CoreGraphics/CGFloat+OctopusKit.swift
@@ -8,36 +8,39 @@
 
 import CoreGraphics
 
+public extension FloatingPoint {
+    
+    /// Shorthand for `Self(Int.random(in: range))`
+    @inlinable
+    static func randomInteger(in range: Range<Int>) -> Self {
+        Self(Int.random(in: range))
+    }
+    
+    /// Shorthand for `Self(Int.random(in: range))`
+    @inlinable
+    static func randomInteger(in range: ClosedRange<Int>) -> Self {
+        Self(Int.random(in: range))
+    }
+    
+    /// Shorthand for `CGFloat(Int.random(in: range, using: &generator))`
+    @inlinable
+    static func randomInteger <T> (in range: Range<Int>, using generator: inout T) -> Self
+        where T: RandomNumberGenerator
+    {
+        Self(Int.random(in: range, using: &generator))
+    }
+    
+    /// Shorthand for `Self(Int.random(in: range, using: &generator))`
+    @inlinable
+    static func randomInteger <T> (in range: ClosedRange<Int>, using generator: inout T) -> Self
+        where T: RandomNumberGenerator
+    {
+        Self(Int.random(in: range, using: &generator))
+    }
+}
+
 public extension CGFloat {
         
-    /// Shorthand for `CGFloat(Int.random(in: range))`
-    @inlinable
-    static func randomInteger(in range: Range<Int>) -> CGFloat {
-        CGFloat(Int.random(in: range))
-    }
-    
-    /// Shorthand for `CGFloat(Int.random(in: range))`
-    @inlinable
-    static func randomInteger(in range: ClosedRange<Int>) -> CGFloat {
-        CGFloat(Int.random(in: range))
-    }
-    
-    /// Shorthand for `CGFloat(Int.random(in: range, using: &generator))`
-    @inlinable
-    static func randomInteger <T> (in range: Range<Int>, using generator: inout T) -> CGFloat
-        where T: RandomNumberGenerator
-    {
-        CGFloat(Int.random(in: range, using: &generator))
-    }
-    
-    /// Shorthand for `CGFloat(Int.random(in: range, using: &generator))`
-    @inlinable
-    static func randomInteger <T> (in range: ClosedRange<Int>, using generator: inout T) -> CGFloat
-        where T: RandomNumberGenerator
-    {
-        CGFloat(Int.random(in: range, using: &generator))
-    }
-    
     /// Treats this value as an angle in radians and returns the radians between it and the specified angle.
     @inlinable
     func deltaBetweenAngle(_ targetAngle: CGFloat) -> Self {
