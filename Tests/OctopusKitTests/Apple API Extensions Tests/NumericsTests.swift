a1a66c7c8d14f5abed88ac7206bc362471d9c2a8
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-12-24 23:53:11 +0400

    Add NumericsTests

---
+++ Tests/OctopusKitTests/Apple API Extensions Tests/NumericsTests.swift
@@ -0,0 +1,78 @@
+//
+//  NumericsTests.swift
+//  OctopusKitTests
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/12/24.
+//
+
+import XCTest
+@testable import OctopusKit
+
+class NumericsTests: XCTestCase {
+
+    func testAdditiveArithmetic() {
+        
+        // Test adjustTowards(_:by:)
+        
+        // #1: Test integers
+        
+        XCTContext.runActivity(named: "Test integers") { _ in
+            var value: Int = 10
+            
+            // #1A: Difference = 1
+            XCTAssertEqual(value.adjustedTowards(0,    by: 1), 9)
+            XCTAssertEqual(value.adjustedTowards(100,  by: 1), 11)
+            
+            // #1B: Difference > 1
+            XCTAssertEqual(value.adjustedTowards(0,    by: 5), 5)
+            XCTAssertEqual(value.adjustedTowards(100,  by: 5), 15)
+            
+            // #1C: Exceeding the target should snap to the target.
+            XCTAssertEqual(value.adjustedTowards(0,    by: 1000), 0)
+            XCTAssertEqual(value.adjustedTowards(100,  by: 1000), 100)
+            
+            // #1D: Negatives
+            value = -10
+            XCTAssertEqual(value.adjustedTowards( 100, by: 1), -9)
+            XCTAssertEqual(value.adjustedTowards(-100, by: 1), -11)
+            
+            // #1E: Mutation
+            value = 10
+            value.adjustTowards(0, by: 1)
+            XCTAssertEqual(value, 9)
+        }
+        
+        // #2: Test floats
+        
+        XCTContext.runActivity(named: "Test integers") { _ in
+            var value: Float = 10.0
+            
+            // #2A: Difference = 0.1
+            XCTAssertEqual(value.adjustedTowards(0.1,   by: 0.1),  9.9)
+            XCTAssertEqual(value.adjustedTowards(100,   by: 0.1),  10.1)
+            
+            // #2B: Difference = 1.0
+            XCTAssertEqual(value.adjustedTowards(0.1,   by: 1.0),  9.0)
+            XCTAssertEqual(value.adjustedTowards(100,   by: 1.0),  11.0)
+            
+            // #2C: Exceeding the target should snap to the target.
+            XCTAssertEqual(value.adjustedTowards(0.1,   by: 10.1), 0.1)
+            XCTAssertEqual(value.adjustedTowards(9.1,   by: 10.1), 9.1)
+            
+            // #2D: Negatives
+            value = -10
+            XCTAssertEqual(value.adjustedTowards(0.1,   by: 0.1), -9.9)
+            XCTAssertEqual(value.adjustedTowards(-90.9, by: 0.1), -10.1)
+            
+            // #2E: Mutation
+            value = 10.0
+            value.adjustTowards(0.1, by: 1)
+            XCTAssertEqual(value, 9.0)
+        }
+        
+    }
+
+    static var allTests = [
+        ("Test AdditiveArithmetic extensions", testAdditiveArithmetic)
+    ]
+}
--- Tests/OctopusKitTests/XCTestManifests.swift
+++ Tests/OctopusKitTests/XCTestManifests.swift
@@ -3,11 +3,14 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
+        testCase(NumericsTests.allTests),
+        testCase(CGPointTests.allTests),
+        testCase(StringTests.allTests),
+        
         testCase(OctopusKitLaunchTests.allTests),
         testCase(OctopusLogTests.allTests),
         testCase(ECSTests.allTests),
-        testCase(StringTests.allTests),
-        testCase(CGPointTests.allTests),
+        
         testCase(ContiguousArray2DTests.allTests)
     ]
 }
