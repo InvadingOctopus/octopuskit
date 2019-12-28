3de3e984b89d40e5f1cfe2c15c5b9fd02eea28d4
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-12-28 09:17:15 +0400

    Create Orientations.swift
    Move from OctopusDirection.swift

---
+++ Sources/OctopusKit/Support & Utility/Enums/Orientations.swift
@@ -0,0 +1,21 @@
+//
+//  Orientations.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/12/28.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import Foundation
+
+public enum OctopusOrientation {
+    case horizontal, vertical
+}
+
+public enum OctopusHorizontalOrientation {
+    case left, right
+}
+
+public enum OctopusVerticalOrientation {
+    case up, down
+}
