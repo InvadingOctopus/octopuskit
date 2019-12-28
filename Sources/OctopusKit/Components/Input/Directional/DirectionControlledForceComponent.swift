5c84dfe9889741ffcec7613383cd9be25f1116d1
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-12-28 12:39:01 +0400

    Remove unused value

--- Sources/OctopusKit/Components/Input/Directional/DirectionControlledForceComponent.swift
+++ Sources/OctopusKit/Components/Input/Directional/DirectionControlledForceComponent.swift
@@ -95,7 +95,6 @@ public final class DirectionControlledForceComponent: OctopusComponent, OctopusU
         
         // ❕ NOTE: Don't use `switch` or `else` because we want to process multiple keypresses, to generate diagonal forces and also cancel out opposing directions.
         
-        let directionsActive = directionEventComponent.directionsActive
         let magnitudeForCurrentUpdate = timestep.applying(magnitude.current, deltaTime: CGFloat(seconds))
         var vector = CGVector.zero
 
