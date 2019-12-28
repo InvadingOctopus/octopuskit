6f1f5f54fa13e522b8855b94d93cdb42beff8ce8
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-12-28 12:49:32 +0400

    Remove OS restriction

--- Sources/OctopusKit/Components/Input/Directional/DirectionControlledTorqueComponent.swift
+++ Sources/OctopusKit/Components/Input/Directional/DirectionControlledTorqueComponent.swift
@@ -14,7 +14,6 @@ import GameplayKit
 /// Set the `LOGINPUTEVENTS` compilation flag to log values.
 ///
 /// **Dependencies:** `DirectionEventComponent`, `PhysicsComponent`
-@available(macOS 10.15, *)
 public final class DirectionControlledTorqueComponent: OctopusComponent, OctopusUpdatableComponent {
     
     // TODO: Tests
