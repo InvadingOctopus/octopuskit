0a6f230c8010faff38a2a6fa0d4c5acf0561b426
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2019-12-28 09:16:39 +0400

    Create FloatingPoint+OctopusKit.swift
    Move from CGFloat+OctopusKit

---
+++ Sources/OctopusKit/Apple API Extensions/Foundation/FloatingPoint+OctopusKit.swift
@@ -0,0 +1,44 @@
+//
+//  FloatingPoint+OctopusKit.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2019/12/28.
+//  Copyright © 2019 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+// TODO: Tests
+
+import Foundation
+
+public extension FloatingPoint {
+     
+    // MARK: - Random Numbers
+    
+    /// Shorthand for `Self(Int.random(in: range))`
+    @inlinable
+    static func randomInteger(in range: Range<Int>) -> Self {
+        Self(Int.random(in: range))
+    }
+    
+    /// Shorthand for `Self(Int.random(in: range))`
+    @inlinable
+    static func randomInteger(in range: ClosedRange<Int>) -> Self {
+        Self(Int.random(in: range))
+    }
+    
+    /// Shorthand for `CGFloat(Int.random(in: range, using: &generator))`
+    @inlinable
+    static func randomInteger <T> (in range: Range<Int>, using generator: inout T) -> Self
+        where T: RandomNumberGenerator
+    {
+        Self(Int.random(in: range, using: &generator))
+    }
+    
+    /// Shorthand for `Self(Int.random(in: range, using: &generator))`
+    @inlinable
+    static func randomInteger <T> (in range: ClosedRange<Int>, using generator: inout T) -> Self
+        where T: RandomNumberGenerator
+    {
+        Self(Int.random(in: range, using: &generator))
+    }
+}
