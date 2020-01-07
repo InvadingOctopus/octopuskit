32a75833fd04bd786a46f44039b426503051f533
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-01-07 18:00:47 +0400

    Update Xcode templates

--- Templates/Xcode/OctopusKit Game State+Scene+UI.xctemplate/___FILEBASENAME___.swift
+++ Templates/Xcode/OctopusKit Game State+Scene+UI.xctemplate/___FILEBASENAME___.swift
@@ -9,16 +9,16 @@ import OctopusKit
 
 final class ___FILEBASENAMEASIDENTIFIER___: OKGameState {
     
-    override var validNextStates: [OKGameState.Type] {
-        // Customize: Specify the valid states that this state can transition to.
-        // NOTE: Do not perform any logic to conditionally control state transitions here. See `OKGameState` documentation.
-        []  // Default: allow all states.
-    }
-    
     init() {
         // NOTE: Game state classes are initialized when the game coordinator is initialized: on game launch.
         super.init(associatedSceneClass:  ___FILEBASENAMEASIDENTIFIER___Scene.self,
                    associatedSwiftUIView: ___FILEBASENAMEASIDENTIFIER___UI())
+    }
+        
+    override var validNextStates: [OKGameState.Type] {
+        // Customize: Specify the valid states that this state can transition to.
+        // NOTE: Do not perform any logic to conditionally control state transitions here. See `OKGameState` documentation.
+        []  // Default: allow all states.
     }
     
 }
