1b63f3393e2444041d9c3981049606900991212b
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-07 04:46:57 +0500

    Update TurnCounterComponent

--- Sources/OctopusKit/Components/Gameplay/Turn-Based/TurnCounterComponent.swift
+++ Sources/OctopusKit/Components/Gameplay/Turn-Based/TurnCounterComponent.swift
@@ -6,27 +6,19 @@
 //  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
 //
 
-// ⚠️ Prototype; Incomplete.
-
 import GameplayKit
 
 public final class TurnCounterComponent: OKTurnBasedComponent {
     
-    public fileprivate(set) var currentTurn: Int = 0
+    public fileprivate(set) var currentTurn:  Int = 0
     public fileprivate(set) var turnsElapsed: Int = 0
     
-    public override func didBeginTurn() {
-        super.didBeginTurn()
-        currentTurn += 1
+    public override func beginTurn(delta turns: Int = 1) {
+        currentTurn += turns
     }
-    
-    public override func executeTurn() {
-        super.executeTurn()
-    }
-    
-    public override func didEndTurn() {
-        super.didEndTurn()
-        turnsElapsed += 1
+
+    public override func endTurn(delta turns: Int = 1) {
+        turnsElapsed += turns
     }
     
 }
