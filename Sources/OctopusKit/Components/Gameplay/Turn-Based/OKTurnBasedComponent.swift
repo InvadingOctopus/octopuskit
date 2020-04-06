c75d37032cf993c031c4f22fc31fd6960b997cfd
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-07 04:42:15 +0500

    Update OKTurnBasedComponent
    - Rename methods
    - Add turn delta parameter

--- Sources/OctopusKit/Components/Gameplay/Turn-Based/OKTurnBasedComponent.swift
+++ Sources/OctopusKit/Components/Gameplay/Turn-Based/OKTurnBasedComponent.swift
@@ -6,24 +6,27 @@
 //  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
 //
 
-// CHECK: Should this be a protocol?
-
-// ⚠️ Prototype; Incomplete.
-
 import GameplayKit
 
 /// An abstract base class for components in a turn-based game.
 ///
-/// Turn-based components may still have a per-frame `update(deltaTime:)` cycle and function like all other components, but they also have special turn-based methods that must be manually called to perform tasks during discrete game-defined turns.
+/// Turn-based components still have a per-frame `update(deltaTime:)` cycle and may perform per-frame updates like all other components, but they also have special methods that must be manually called to perform tasks for each discrete game-defined turn.
 open class OKTurnBasedComponent: OKComponent {
 
-    /// Abstract; override in subclass.
-    open func didBeginTurn() {}
+    // DESIGN: CHECKED: Should this be a protocol with default implementations? No, as it would not be consistent with the `override` for regular `update(deltaTime:)` etc. :)
+
+    /// *Abstract; override in subclass.* Use this to perform any tasks that must occur at the beginning of each turn, *before* `updateTurn(delta:)`, such as health regeneration effects.
+    ///
+    /// - Parameter turns: The number of turns passed since the previous update. Default: `1`
+    open func beginTurn(delta turns: Int = 1) {}
     
-    /// Abstract; override in subclass.
-    open func executeTurn() {}
+    /// *Abstract; override in subclass.*
+    /// - Parameter turns: The number of turns passed since the previous update. Default: `1`
+    open func updateTurn(delta turns: Int = 1) {}
     
-    /// Abstract; override in subclass.
-    open func didEndTurn() {}
+    /// *Abstract; override in subclass.* Use this to perform any tasks that must occur at the end of each turn, *after* `updateTurn(delta:)`, such as damage-over-time effects.
+    ///
+    /// - Parameter turns: The number of turns passed since the previous update. Default: `1`
+    open func endTurn(delta turns: Int = 1) {}
     
 }
