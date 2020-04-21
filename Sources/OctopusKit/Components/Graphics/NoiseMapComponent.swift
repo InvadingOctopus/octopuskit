070b55d59f4a1264cfbe6e85ac456266b69b618d
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-21 14:09:53 +0500

    Add NoiseMapComponent

---
+++ Sources/OctopusKit/Components/Graphics/NoiseMapComponent.swift
@@ -0,0 +1,120 @@
+//
+//  NoiseMapComponent.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2020/04/21.
+//  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import GameplayKit
+
+/// Encapsulates a `GKNoiseMap` object which uses a `GKNoise` field from a `NoiseComponent`.
+///
+/// **Dependencies:** `NoiseComponent`
+public class NoiseMapComponent: OKComponent {
+
+    public override var requiredComponents: [GKComponent.Type]? {
+        [NoiseComponent.self]
+    }
+    
+    /// A noise map, created when this component is added to an entity with a `NoiseComponent`.
+    public private(set) var noiseMap: GKNoiseMap? {
+        didSet { // Update this component's properties to match the noise map's properties.
+            // CHECK: Will this be the expected behavior?
+            if  let noiseMap = noiseMap {
+                self.size           = noiseMap.size
+                self.origin         = noiseMap.origin
+                self.sampleCount    = noiseMap.sampleCount
+                self.isSeamless     = noiseMap.isSeamless
+            }
+        }
+    }
+    
+    /// The size of the “slice” of noise samples contained in the noise map relative to the unit coordinate space of the noise object it was created from. Regenerates the noise map when modified.
+    public var size: vector_double2 {
+        didSet {
+            if  noiseMap != nil,
+                size     != oldValue
+            {
+                generateNoiseMap()
+            }
+        }
+    }
+    
+    /// The position of the “slice” of noise samples contained in the noise map relative to the unit coordinate space of the noise object it was created from. Regenerates the noise map when modified.
+    public var origin: vector_double2 {
+        didSet {
+            if  noiseMap != nil,
+                origin   != oldValue
+            {
+                generateNoiseMap()
+            }
+        }
+    }
+    
+    /// The width and height of integer grid for which the noise map contains sampled noise values. Regenerates the noise map when modified.
+    public var sampleCount: vector_int2 {
+           didSet {
+               if  noiseMap    != nil,
+                   sampleCount != oldValue
+               {
+                   generateNoiseMap()
+               }
+           }
+       }
+    
+    /// Indicates whether the noise map’s output can repeat seamlessly in all directions. Regenerates the noise map when modified.
+    public var isSeamless: Bool {
+           didSet {
+               if  noiseMap   != nil,
+                   isSeamless != oldValue
+               {
+                   generateNoiseMap()
+               }
+           }
+       }
+    
+    public override func didAddToEntity() {
+        generateNoiseMap()
+    }
+    
+    
+    /// Initializes a `NoiseMapComponent` with the parameters for creating a `GKNoiseMap` when this component is added to an entity with a `NoiseComponent`.
+    /// - Parameters:
+    ///   - size: The size of 2D “slice” to take from the unit coordinate space of the noise object.
+    ///   If you later generate a texture image from the noise map with the `SKTexture` class, this size is also the pixel dimensions of the texture to generate.
+    ///   - origin: The position of the 2D “slice” to take from the unit coordinate space of the noise object.
+    ///   - sampleCount: The width and height of integer grid for sampling noise values from the noise object.
+    ///   - seamless: `true` to adjust samples taken from the noise object so that generated texture images can be tiled without visible seams; `false` otherwise.
+    public init(size:           vector_double2,
+                origin:         vector_double2,
+                sampleCount:    vector_int2,
+                seamless:       Bool)
+    {
+        self.size           = size
+        self.origin         = origin
+        self.sampleCount    = sampleCount
+        self.isSeamless     = seamless
+        super.init()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    public override func didAddToEntity(withNode node: SKNode) {
+        generateNoiseMap()
+    }
+    
+    /// Creates a new noise map, using the properties of this component and the noise field from a `NoiseComponent`, replacing the existing map if any.
+    @discardableResult
+    func generateNoiseMap() -> GKNoiseMap? {
+        guard let noiseComponent = coComponent(NoiseComponent.self) else { return self.noiseMap }
+        
+        self.noiseMap = GKNoiseMap(noiseComponent.noise,
+                                   size:         self.size,
+                                   origin:       self.origin,
+                                   sampleCount:  self.sampleCount,
+                                   seamless:     self.isSeamless)
+        return self.noiseMap
+    }
+}
+
