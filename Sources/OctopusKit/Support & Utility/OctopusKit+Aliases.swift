c0fd4884cf89da592a6df0da05bf672e99844f72
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-23 12:27:37 +0500

    Add shorter aliases for common types

---
+++ Sources/OctopusKit/Support & Utility/OctopusKit+Aliases.swift
@@ -0,0 +1,17 @@
+//
+//  OctopusKit+Aliases.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2020-04-22
+//  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+// Some cleaner aliases without prefixes for commonly-used types.
+
+// NOTE: The engine API should always use the original types for specific clarity. These aliases are provided for the engine's clients.
+
+import Foundation
+
+public typealias Point  = CGPoint
+public typealias Size   = CGSize
+public typealias Vector = CGVector
