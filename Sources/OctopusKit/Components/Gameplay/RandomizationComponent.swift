ae915e596948bb6fa8443576faf22d1f429e3a12
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-23 18:54:15 +0500

    Update RandomizationComponent
    - Rename init parameter
    - Change default argument to GKRandomSource
    - Add RandomNumberGenerator conformance. TODO: Verify

--- Sources/OctopusKit/Components/Gameplay/RandomizationComponent.swift
+++ Sources/OctopusKit/Components/Gameplay/RandomizationComponent.swift
@@ -13,15 +13,33 @@ open class RandomizationComponent: OKComponent {
     
     open var source: GKRandom
     
-    public init(randomizationSource: GKRandom = GKRandomDistribution(randomSource: GKARC4RandomSource(),
-                                                                     lowestValue:  0,
-                                                                     highestValue: 1))
-    {
-        self.source = randomizationSource
+    public init(source: GKRandom = GKRandomSource()) {
+        self.source = source
         super.init()
     }
     
     public required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
 }
 
+extension RandomizationComponent: RandomNumberGenerator {
+    
+    // TODO: Verify & Test
+    
+    /// Calls `source.next()`
+    ///
+    /// - Returns: An unsigned 64-bit random value.
+    public func next<T>() -> T
+        where T: FixedWidthInteger, T: UnsignedInteger
+    {
+        return T(abs(self.source.nextInt()))
+    }
+    
+    /// Calls `source.next(upperBound:)`
+    ///
+    /// - Returns - A random value of T in the range 0..<upperBound. Every value in the range 0..<upperBound is equally likely to be returned.
+    public func next<T>(upperBound: T) -> T
+        where T: FixedWidthInteger, T: UnsignedInteger
+    {
+        return T(abs(self.source.nextInt(upperBound: Int(upperBound))))
+    }
+}
