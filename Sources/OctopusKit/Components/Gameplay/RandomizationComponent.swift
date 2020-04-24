0fda0dcb12c51b793955e6cb1a38a96b1341d43f
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-24 13:03:15 +0500

    Fix next() default implementation
    TODO: Verify and test

--- Sources/OctopusKit/Components/Gameplay/RandomizationComponent.swift
+++ Sources/OctopusKit/Components/Gameplay/RandomizationComponent.swift
@@ -27,15 +27,23 @@ extension RandomizationComponent: RandomNumberGenerator {
     
     // ⚠️ WARNING: GKRandomDistribution does not currently work when accessed as a RandomNumberGenerator
     
-    /// Calls `source.next()`
+    /// Calls `source.next()` if `source` conforms to `RandomNumberGenerator`, otherwise calls `source.nextInt()` twice to generate an unsigned 64-bit integer.
     ///
     /// - Returns: An unsigned 64-bit random value.
-    public func next<T>() -> T
-        where T: FixedWidthInteger, T: UnsignedInteger
-    {
-        return T(abs(self.source.nextInt()))
+    public func next() -> UInt64 {
+        if  var source = self.source as? RandomNumberGenerator {
+            return source.next()
+        } else {
+            // CREDIT: Grigory Entin https://stackoverflow.com/users/1859783/grigory-entin
+            // https://stackoverflow.com/a/57370987/1948215
+            // GKRandom produces values in the `INT32_MIN...INT32_MAX` range; hence we need two numbers to produce 64-bit value.
+            let next1 = UInt64(bitPattern: Int64(self.source.nextInt()))
+            let next2 = UInt64(bitPattern: Int64(self.source.nextInt()))
+            return next1 ^ (next2 << 32)
+        }
     }
     
+    /*
     /// Calls `source.next(upperBound:)`
     ///
     /// - Returns - A random value of T in the range 0..<upperBound. Every value in the range 0..<upperBound is equally likely to be returned.
@@ -44,4 +52,5 @@ extension RandomizationComponent: RandomNumberGenerator {
     {
         return T(abs(self.source.nextInt(upperBound: Int(upperBound))))
     }
+    */
 }
