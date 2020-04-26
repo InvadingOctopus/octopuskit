a41a06fe16e60b818788afab82281201d8deb3e5
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-26 20:30:20 +0500

    Add CaseIterable extensions
    - CaseIterable.random() and .random(using:)

---
+++ Sources/OctopusKit/Apple API Extensions/Foundation/CaseIterable+OctopusKit.swift
@@ -0,0 +1,20 @@
+//
+//  CaseIterable+OctopusKit.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2020/04/26.
+//  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import Foundation
+
+public extension CaseIterable {
+    
+    static func randomElement() -> Self? {
+        Self.allCases.randomElement()
+    }
+    
+    static func randomElement<T>(using generator: inout T) -> Self? where T : RandomNumberGenerator {
+        Self.allCases.randomElement(using: &generator)
+    }
+}
