c03eb31fa0605e29b009aea70ea86fe03b2dfff2
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-28 21:51:04 +0500

    Fix DirectionControlledTileBasedPositioningComponent
    - Correct implementation
    - Add stepMultiplier
    - Add documentation
    - Name stays the same :)

--- Sources/OctopusKit/Components/Input/Directional/DirectionControlledTileBasedPositioningComponent.swift
+++ Sources/OctopusKit/Components/Input/Directional/DirectionControlledTileBasedPositioningComponent.swift
@@ -8,38 +8,42 @@
 
 import GameplayKit
 
+/// Changes the coordinates of the entity's `TileBasedPositionComponent` based on player input via a `DirectionEventComponent`.
 open class DirectionControlledTileBasedPositioningComponent: OKComponent, OKUpdatableComponent {
     
     open override var requiredComponents: [GKComponent.Type]? {
-        [DirectionEventComponent.self]
+        [DirectionEventComponent.self,
+         TileBasedPositionComponent.self]
     }
     
+    /// The property of the `DirectionEventComponent` to monitor for input, such as `directionsBeganForCurrentFrame` or `directionsActive`. May be overriden in a subclass to effectively chooses whether to move once (e.g. when a key is first pressed) or to move repeatedly until an input ends (e.g. until a key is lifted).
     @inlinable
     open var directionsSource: Set<OKInputDirection>? {
         guard let directionEventComponent = coComponent(DirectionEventComponent.self) else { return nil }
-        directionEventComponent.directionsBeganForCurrentFrame
+        return directionEventComponent.directionsBeganForCurrentFrame
     }
     
-    /// The number of tiles to move in.
-    var stepMultiplier: Int = 1
+    /// The number of tiles to move in for each input event.
+    open var stepMultiplier: CGFloat = 1
     
-    @inlinable
     open override func update(deltaTime seconds: TimeInterval) {
         guard
             let tileBasedPositionComponent = coComponent(TileBasedPositionComponent.self),
             let input = directionsSource
-        else { return }
-        
+            else { return }
+
         var newCoordinates = tileBasedPositionComponent.coordinates
-        
+
         // ❕ NOTE: Use `if` instead of `switch` so all directions are processed, to allow diagonals and to let opposing directions cancel each other out.
-        
-        if input.contains(.up)      { newCoordinates.y += 1 * stepMultiplier }
-        if input.contains(.down)    { newCoordinates.y -= 1 * stepMultiplier }
-        if input.contains(.right)   { newCoordinates.x += 1 * stepMultiplier }
-        if input.contains(.left)    { newCoordinates.x -= 1 * stepMultiplier }
-        
-        tileBasedPositionComponent = newCoordinates
+
+        if input.contains(.up)      { newCoordinates.y += 1 }
+        if input.contains(.down)    { newCoordinates.y -= 1 }
+        if input.contains(.right)   { newCoordinates.x += 1 }
+        if input.contains(.left)    { newCoordinates.x -= 1 }
+
+        newCoordinates *= stepMultiplier
+    
+        tileBasedPositionComponent.coordinates = newCoordinates
         tileBasedPositionComponent.clampCoordinates()
     }
 }
