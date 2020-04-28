b8486fb4570d089bc4fedb97a092b7abe77b9674
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-04-28 23:12:16 +0500

    Fix Comparable.clamped(to:) for Range
    - Because half-open range expressions like 0..<10 still have 10 as the upperBound, but the expected behavior would be to max out at 9.
    - Of course we cannot just upperBound - 1 for floating point (see comments) so this is only implemented for BinaryInteger.

--- Sources/OctopusKit/Apple API Extensions/Foundation/Comparable+OctopusKit.swift
+++ Sources/OctopusKit/Apple API Extensions/Foundation/Comparable+OctopusKit.swift
@@ -6,19 +6,20 @@
 //  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
 //
 
+// TODO: Eliminate code duplication for clamped(to:) etc.?
+// CHECK: Should we check for empty ranges in clamped(to:) etc.?
+
 import Foundation
 
 public extension Comparable {
     
     // NOTE: An extension to the `Comparable` protocol applies to `Int`, `Float`, `Double`, etc.
     
-    // TODO: Eliminate code duplication for clamped(to:) etc.?
-    // CHECK: Should we check for empty ranges in clamped(to:) etc.?
-    
     /// Returns a value which is equal to this value limited to the specified range.
     @inlinable
     func clamped(to range: ClosedRange<Self>) -> Self {
         // CREDIT: https://stackoverflow.com/a/43769799/1948215
+        // NOTE: This does not need `upperBound - 1` as for `Range` because 0...10 must include 10.
         min(max(self, range.lowerBound), range.upperBound)
     }
     
@@ -27,12 +28,16 @@ public extension Comparable {
     mutating func clamp(to range: ClosedRange<Self>) {
         self = self.clamped(to: range)
     }
+}
+
+public extension Comparable where Self: BinaryInteger {
     
     /// Returns a value which is equal to this value limited to the specified range.
     @inlinable
     func clamped(to range: Range<Self>) -> Self {
         // CREDIT: https://stackoverflow.com/a/43769799/1948215
-        min(max(self, range.lowerBound), range.upperBound)
+        // NOTE: We cannot implement this for floating point because the upper bound of `0 ..< 8.5 - 1` would be 7.5 not 8.4
+        min(max(self, range.lowerBound), range.upperBound - 1) // ❕ Because the upperBound of `0 ..< 10` woud be 10
     }
     
     /// Limits this value to the specified range.
