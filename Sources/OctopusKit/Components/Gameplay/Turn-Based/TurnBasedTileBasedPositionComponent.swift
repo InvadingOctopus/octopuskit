2fc64a26557c1b183559603529db533a30c343d2
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-02 16:06:33 +0500

    Add TurnBasedTileBasedPositionComponent
    Another long name, perhaps to be shortened later ^-^

---
+++ Sources/OctopusKit/Components/Gameplay/Turn-Based/TurnBasedTileBasedPositionComponent.swift
@@ -0,0 +1,34 @@
+//
+//  TurnBasedTileBasedPositionComponent.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2020/05/02.
+//  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import GameplayKit
+
+/// Updates the coordinates of the entity's `TileBasedPositionComponent` on a new game turn, ultimately setting the position of the entity's `SpriteKitComponent` node.
+open class TurnBasedTileBasedPositionComponent: OKTurnBasedComponent {
+        
+    open override var requiredComponents: [GKComponent.Type]? {
+        [TileBasedPositionComponent.self]
+    }
+
+    /// The coordinates to set on the entity's `TileBasedPositionComponent` in `updateTurn(delta:)`.
+    var pendingCoordinates: CGPoint?
+    
+    open override func updateTurn(delta turns: Int) {
+        guard
+            let pendingCoordinates = self.pendingCoordinates,
+            let tileBasedPositionComponent = coComponent(TileBasedPositionComponent.self)
+            else { return }
+        
+        tileBasedPositionComponent.coordinates = pendingCoordinates
+        
+        // Clear the pending coordinates so they don't get reapplied.
+        
+        self.pendingCoordinates = nil
+    }
+
+}
