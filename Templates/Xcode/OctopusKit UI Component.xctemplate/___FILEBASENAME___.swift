6d4033011814015fb1c9d0ddf1ad9ab19c05a812
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-02 18:31:24 +0500

    Add UI Component Xcode template

---
+++ Templates/Xcode/OctopusKit UI Component.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,52 @@
+//___FILEHEADER___
+
+import SpriteKit
+import GameplayKit
+import OctopusKit
+import SwiftUI
+
+/// A component with an associated SwiftUI view.
+final class ___FILEBASENAMEASIDENTIFIER___: OKComponent, OKUpdatableComponent, ObservableObject  {
+    
+    override var requiredComponents: [GKComponent.Type]? {
+        []
+    }
+    
+    // MARK: Observed Properties
+    
+    @Published var frame: Int = 0 // CUSTOMIZE
+    
+    // MARK: Life Cycle
+    
+    override func didAddToEntity(withNode node: SKNode) {
+        
+    }
+    
+    override func update(deltaTime seconds: TimeInterval) {
+        self.frame += 1 // CUSTOMIZE
+    }
+    
+    override func willRemoveFromEntity(withNode node: SKNode) {
+        
+    }
+    
+    // MARK: - UI
+    
+    /// Creates and returns a copy of this component's associated SwiftUI view, with this instance of the component as the view's observed property.
+    var ui: ___FILEBASENAMEASIDENTIFIER___.UI {
+        // NOTE: Returning a singleton may not make a difference in performance, as SwiftUI Views are value types (that would be copied anyway).
+        ___FILEBASENAMEASIDENTIFIER___.UI(component: self)
+    }
+    
+    /// A SwiftUI view that displays UI based on the data of a ___FILEBASENAMEASIDENTIFIER___.
+    struct UI: View {
+        
+        @ObservedObject var component: ___FILEBASENAMEASIDENTIFIER___
+        
+        var body: some View {
+            Text("\(component.frame)") // CUSTOMIZE
+        }
+        
+    }
+    
+}
