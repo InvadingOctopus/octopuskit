530b4f6a22b91e47a246597c15e5cfc4c6523c86
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-07 02:07:07 +0500

    Improve TileBasedPositionComponent
    - Add animation
    - Update documentation; begin referring to SpriteKitComponent as NodeComponent \^\^

--- Sources/OctopusKit/Components/Graphics/TileBasedPositionComponent.swift
+++ Sources/OctopusKit/Components/Graphics/TileBasedPositionComponent.swift
@@ -7,11 +7,16 @@
 //
 
 import SpriteKit
+import GameplayKit
 
-/// Updates the entity's `SpriteKitComponent` node's position in every frame to align with the center of a tile in a `TileMapComponent`.
+/// Updates the entity's `NodeComponent` node's position in every frame to align with the center of a tile in a `TileMapComponent`.
 public final class TileBasedPositionComponent: OKComponent, OKUpdatableComponent {
     
     // TODO: Warp-around option.
+    
+    public override var requiredComponents: [GKComponent.Type]? {
+        [NodeComponent.self]
+    }
     
     /// Specifies a `TileMapComponent` or its subclass which may be in another entity, e.g. a map entity. If `nil` then this component's entity's `TileMapComponent` (but not a subclass) is used, if any.
     ///
@@ -24,39 +29,54 @@ public final class TileBasedPositionComponent: OKComponent, OKUpdatableComponent
     /// Applies an offset to the entity's node position from the center of a map tile.
     public var offsetFromTileCenter: CGPoint = .zero
     
-    /// The coordinates of a tile in the `TileMapComponent`'s `SKTileMapNode`. The entity's `SpriteKitComponent` node's position is set to this tile's center in every frame, adding the `offsetFromTileCenter`.
+    /// The coordinates of a tile in the `TileMapComponent`'s `SKTileMapNode`. The entity's node's position is set to this tile's center in every frame, adding the `offsetFromTileCenter`.
     public var coordinates: CGPoint = .zero {
         didSet {
             if  coordinates != oldValue {
-                shouldAlignNodePositionOnUpdate = true
+                alignNodePositionOnUpdate = true
             }
         }
     }
     
     /// If `true`, `alignNodePositionToTile()` is called when this component is updated, after which this flag is reset. Automatically set when `coordinates` are modified.
     ///
-    /// If the entity's `SpriteKitComponent` node's position is modified by other components, then this flag must be set to `true` or `alignNodePositionToTile()` must be called manually to re-align the node with its map tile.
-    public var shouldAlignNodePositionOnUpdate: Bool = false
+    /// If the entity's node's position is modified by other components, then this flag must be set to `true` or `alignNodePositionToTile()` must be called manually to re-align the node with its map tile.
+    public var alignNodePositionOnUpdate: Bool = false
+    
+    /// If `true`, an `SKAction` animates the entity's node from its current position to the tile's position. If `false`, the node is moved to the new position immediately.
+    public var animate:             Bool
+    
+    public var animationDuration:   TimeInterval
+    public var animationTimingMode: SKActionTimingMode
+    
+    public var animationActionKey   = "OctopusKit.Animation.TileBasedPositionComponent"
     
     /// - Parameters:
     ///   - tileMapComponentOverride: Specify `nil` to use this component's entity's `TileMapComponent`, or specify a `TileMapComponent` (or its subclass) in another entity, e.g. a map entity.
     ///
     ///     A `RelayComponent` in this component's entity may also be used to connect to a `TileMapComponent` in another entity by leaving this property `nil`.
     ///
-    ///   - coordinates: The coordinates of the initial tile in the `TileMapComponent`'s `SKTileMapNode`. The entity's `SpriteKitComponent` node's position is set to this tile's center in every frame.
+    ///   - coordinates: The coordinates of the initial tile in the `TileMapComponent`'s `SKTileMapNode`. The entity's node's position is set to this tile's center in every frame.
     ///   - tileMapLayer: The index of the `TileMapComponent` layer to use, i.e. the `SKTileMapNode` to query for the tile coordinates.
-    ///   - offsetFromTileCenter: The additional offset to apply to the entity's `SpriteKitComponent` node.
-    public init(tileMapComponentOverride:   TileMapComponent? = nil,
-                tileMapLayer:               Int = 0,
-                coordinates:                CGPoint = .zero,
-                offsetFromTileCenter:       CGPoint = .zero)
+    ///   - offsetFromTileCenter: The additional offset to apply to the entity's node.
+    ///   - animate: If `true`, an `SKAction` animates the entity's node from its current position to the tile's position. If `false`, the node is moved to the new position immediately.
+    public init(tileMapComponentOverride:   TileMapComponent?   = nil,
+                tileMapLayer:               Int                 = 0,
+                coordinates:                CGPoint             = .zero,
+                offsetFromTileCenter:       CGPoint             = .zero,
+                animate:                    Bool                = true,
+                animationDuration:          TimeInterval        = 0.2,
+                animationTimingMode:        SKActionTimingMode  = .easeIn)
     {
-        self.tileMapComponentOverride   = tileMapComponentOverride
-        self.tileMapLayer               = tileMapLayer
-        self.coordinates                = coordinates
-        self.offsetFromTileCenter       = offsetFromTileCenter
+        self.tileMapComponentOverride       = tileMapComponentOverride
+        self.tileMapLayer                   = tileMapLayer
+        self.coordinates                    = coordinates
+        self.offsetFromTileCenter           = offsetFromTileCenter
+        self.animate                        = animate
+        self.animationDuration              = animationDuration
+        self.animationTimingMode            = animationTimingMode
+        self.alignNodePositionOnUpdate      = true // To align with the initial coordinates.
         super.init()
-        self.shouldAlignNodePositionOnUpdate = true // To align with the initial coordinates.
     }
     
     public required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
@@ -68,13 +88,13 @@ public final class TileBasedPositionComponent: OKComponent, OKUpdatableComponent
     
     @inlinable
     public override func update(deltaTime seconds: TimeInterval) {
-        if  shouldAlignNodePositionOnUpdate { // PERFORMANCE: No unnecessary updates.
+        if  alignNodePositionOnUpdate { // PERFORMANCE: No unnecessary updates.
             alignNodePositionToTile()
-            shouldAlignNodePositionOnUpdate = false
+            alignNodePositionOnUpdate = false
         }
     }
     
-    /// Sets the position of this entity's `SpriteKitComponent` node to the center of the tile in the `tileMapComponent`'s `SKTileMapNode`.
+    /// Sets the position of this entity's `NodeComponent` node to the center of the tile in the `tileMapComponent`'s `SKTileMapNode`.
     @inlinable
     public func alignNodePositionToTile() {
         guard
@@ -105,12 +125,26 @@ public final class TileBasedPositionComponent: OKComponent, OKUpdatableComponent
         
         let tiledPosition = tileMapNode.centerOfTile(atColumn: column, row: row) + offsetFromTileCenter
         
+        // Set the position inside the appropriate coordinate space.
+        
+        let newNodePosition: CGPoint
+        
         if  let nodeParent = node.parent { // CHECK & VERIFY & TEST: Is this the expected behavior?
-            node.position = tileMapNode.convert(tiledPosition, to: nodeParent)
+            newNodePosition = tileMapNode.convert(tiledPosition, to: nodeParent)
         } else if let tileMapParent = tileMapNode.parent {
-            node.position = tileMapNode.convert(tiledPosition, to: tileMapParent)
+            newNodePosition = tileMapNode.convert(tiledPosition, to: tileMapParent)
         } else {
-            node.position = tiledPosition
+            newNodePosition = tiledPosition
+        }
+        
+        // Finally! Move the node to new position.
+        
+        if animate {
+            let moveAction = SKAction.move(to: newNodePosition, duration: animationDuration)
+                .withTimingMode(animationTimingMode)
+            node.run(moveAction) // NOTE: Applying `withKey: animationActionKey` makes the animation feel unnatural.
+        } else {
+            node.position = newNodePosition
         }
     }
     
