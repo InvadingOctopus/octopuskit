cd067448d35fc1a8f77eb7e67f73849ac1d7b132
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-09 05:15:08 +0500

    Improve DirectionEventComponent
    - Add DirectionEventSource.manual
    - Add manualDirections and previousManualDirections
    - Add placeholder DirectionEventSource cases
    - Add copyManualEvents()
    - Add documentation

--- Sources/OctopusKit/Components/Input/Directional/DirectionEventComponent.swift
+++ Sources/OctopusKit/Components/Input/Directional/DirectionEventComponent.swift
@@ -16,14 +16,31 @@ public typealias OctopusInputDirection = OKInputDirection
 /// Provides an abstraction layer for directional input from a keyboard, gamepad, joystick, or similar controllers.
 public final class DirectionEventComponent: OKComponent, OKUpdatableComponent {
     
+    // TODO: Support for multiple keys for each direction (e.g. WASD and arrows)
+    
     // MARK: - Subtypes
 
     /// A controller with directional inputs.
     public enum DirectionEventSource {
         case keyboard
-        // TODO: case gamepad
-        // TODO: case joystick
-        // TODO: case onscreen
+        
+        /// A discrete digital input source.
+        ///
+        /// - WARNING: NOT IMPLEMENTED
+        case gamepad
+        
+        /// An analog directional input source.
+        ///
+        /// - WARNING: NOT IMPLEMENTED
+        case joystick
+        
+        /// Onscreen buttons.
+        ///
+        /// - WARNING: NOT IMPLEMENTED
+        case onscreen
+        
+        /// Specifies a custom source of direction events implemented via code that manually modifies the `manualDirections` property.
+        case manual
     }
     
     // MARK: - Properties
@@ -70,6 +87,16 @@ public final class DirectionEventComponent: OKComponent, OKUpdatableComponent {
         directionsActive.reduce(.zero, +)
     }
     
+    /// The `manualDirections` set is copied to this property when modified, and compared against to determine which events began in the current frame and which events ended. Cleared at the end of every frame update.
+    public var previousManualDirections: Set<OKInputDirection> = []
+    
+    /// When `DirectionEventSource.manual` is used, this property can be manually modified to inject code via logic in code. Cleared at the end of every frame update.
+    public var manualDirections: Set<OKInputDirection> = [] {
+        didSet {
+            previousManualDirections = oldValue
+        }
+    }
+    
     public init(source: DirectionEventSource) {
         self.eventSource = source
         super.init()
@@ -100,7 +127,50 @@ public final class DirectionEventComponent: OKComponent, OKUpdatableComponent {
         
         switch self.eventSource {
         case .keyboard: copyKeyboardEvents()
+        case .manual:   copyManualEvents()
+        default: break
         }
+    }
+    
+    /// Clears all events.
+    @inlinable
+    public func clearLists() {
+        // CHECK: PERFORMANCE: Should we be `keepingCapacity`?
+        directionsBeganForCurrentFrame  .removeAll(keepingCapacity: true)
+        directionsEndedForCurrentFrame  .removeAll(keepingCapacity: true)
+    }
+    
+    @inlinable
+    public override func willRemoveFromEntity() {
+        super.willRemoveFromEntity()
+        directionsBeganForCurrentFrame  .removeAll(keepingCapacity: false)
+        directionsEndedForCurrentFrame  .removeAll(keepingCapacity: false)
+        directionsActive                .removeAll(keepingCapacity: false)
+        manualDirections                .removeAll(keepingCapacity: false)
+        previousManualDirections        .removeAll(keepingCapacity: false)
+    }
+    
+    // MARK: - Input Sources
+    
+    /// Generates `directionsBeganForCurrentFrame` and `directionsEndedForCurrentFrame` from `manualDirections` and `previousManualDirections`, then clears the manual direction sets.
+    @inlinable
+    public func copyManualEvents() {
+        guard  !manualDirections.isEmpty
+            || !previousManualDirections.isEmpty
+            else { return}
+        
+        // Events that are in `manualDirections` but not in `previousManualDirections`
+        
+        directionsBeganForCurrentFrame = manualDirections.subtracting(previousManualDirections)
+        
+        // Events that are in `previousManualDirections` but not in `manualDirections`
+        
+        directionsEndedForCurrentFrame = previousManualDirections.subtracting(manualDirections)
+        
+        // Clear the lists so they don't keep repeating every frame unless manually re-added.
+        
+        manualDirections.removeAll(keepingCapacity: true)
+        previousManualDirections.removeAll(keepingCapacity: true)
     }
     
     #if canImport(AppKit)
@@ -137,21 +207,5 @@ public final class DirectionEventComponent: OKComponent, OKUpdatableComponent {
     /// An empty, dummy function if not running on macOS.
     @inlinable public func copyKeyboardEvents() {}
     #endif
-    
-    /// Clears all events.
-    @inlinable
-    public func clearLists() {
-        // CHECK: PERFORMANCE: Should we be `keepingCapacity`?
-        directionsBeganForCurrentFrame  .removeAll(keepingCapacity: true)
-        directionsEndedForCurrentFrame  .removeAll(keepingCapacity: true)
-    }
-    
-    @inlinable
-    public override func willRemoveFromEntity() {
-        super.willRemoveFromEntity()
-        directionsBeganForCurrentFrame  .removeAll(keepingCapacity: false)
-        directionsEndedForCurrentFrame  .removeAll(keepingCapacity: false)
-        directionsActive                .removeAll(keepingCapacity: false)
-    }
 }
 
