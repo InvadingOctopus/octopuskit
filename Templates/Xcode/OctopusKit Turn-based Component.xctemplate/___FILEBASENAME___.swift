33bae84712b7a1b6c10cdb54574884824f10fc4a
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-14 01:18:50 +0500

    Add Turn-based Component Xcode template

---
+++ Templates/Xcode/OctopusKit Turn-based Component.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,26 @@
+//___FILEHEADER___
+
+import SpriteKit
+import GameplayKit
+import OctopusKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: OKTurnBasedComponent {
+    
+    override var requiredComponents: [GKComponent.Type]? {
+        []
+    }
+    
+    override func beginTurn(delta turns: Int = 1) {
+        
+    }
+    
+    override func updateTurn(delta turns: Int = 1) {
+        
+    }
+    
+    override func endTurn(delta turns: Int = 1) {
+        
+    }
+    
+}
+
