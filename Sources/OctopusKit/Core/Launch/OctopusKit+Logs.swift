2b6f4b27febca514b3e5710b10fdcc9e900664d9
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-20 04:17:54 +0500

    Reduce clutter

--- Sources/OctopusKit/Core/Launch/OctopusKit+Logs.swift
+++ Sources/OctopusKit/Core/Launch/OctopusKit+Logs.swift
@@ -8,39 +8,39 @@
 
 //  Logs are in a separate extension for convenience, e.g. so that a project may replace them with its own versions.
 
-extension OctopusKit {
+public extension OctopusKit {
     
     // MARK: Global Logs
     
     /// A log for core or general engine events.
-    public static var logForFramework   = OKLog(title: "🐙")
+    static var logForFramework  = OKLog(title: "🐙")
     
     /// A log for transitions within game states and entity states.
-    public static var logForStates      = OKLog(title: "🐙🚦", suffix: "🚦")
+    static var logForStates     = OKLog(title: "🐙🚦", suffix: "🚦")
     
     /// A log for the components architecture, including entities and component systems.
-    public static var logForComponents  = OKLog(title: "🐙🧩", suffix: "🧩")
+    static var logForComponents = OKLog(title: "🐙🧩", suffix: "🧩")
     
     /// A log for operations that involve loading, downloading, caching and writing game assets and related resources.
-    public static var logForResources   = OKLog(title: "🐙📦", suffix: "📦")
+    static var logForResources  = OKLog(title: "🐙📦", suffix: "📦")
     
     /// A log for the cycle of turn updates in a turn-based game.
-    public static var logForTurns       = OKLog(title: "🐙🔄", suffix: "🔄")
+    static var logForTurns      = OKLog(title: "🐙🔄", suffix: "🔄")
     
     /// A log for deinitializations; when an object is freed from memory.
-    public static var logForDeinits     = OKLog(title: "🐙💀", suffix: "💀")
+    static var logForDeinits    = OKLog(title: "🐙💀", suffix: "💀")
     
     /// A log for events that may cause unexpected behavior but do not prevent the continued execution of the game.
-    public static var logForWarnings    = OKLog(title: "🐙⚠️", suffix: "⚠️")
+    static var logForWarnings   = OKLog(title: "🐙⚠️", suffix: "⚠️")
     
     /// A log for severe, unexpected events that may prevent the continued execution of the game.
     ///
     /// - Warning: Adding an entry to this log will raise a `fatalError` that terminates the application.
-    public static var logForErrors      = OKLog(title: "🐙🚫", suffix: "🚫", haltApplicationOnNewEntry: true)
+    static var logForErrors     = OKLog(title: "🐙🚫", suffix: "🚫", haltApplicationOnNewEntry: true)
     
     /// A log for verbose debugging information.
-    public static var logForDebug       = OKLog(title: "🐙🐞")
+    static var logForDebug      = OKLog(title: "🐙🐞")
     
     /// A log for developer tips to assist with fixing warnings and errors.
-    public static var logForTips        = OKLog(title: "🐙💡")
+    static var logForTips       = OKLog(title: "🐙💡")
 }
