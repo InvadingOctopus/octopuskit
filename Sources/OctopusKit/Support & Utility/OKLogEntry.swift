bf47880d207e633000409d94bf616eb403d8f059
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-21 22:00:33 +0500

    Exclude id from OKLogEntry coding
    Long and unnecessary strings.

--- Sources/OctopusKit/Support & Utility/OKLogEntry.swift
+++ Sources/OctopusKit/Support & Utility/OKLogEntry.swift
@@ -11,7 +11,7 @@ import Foundation
 public typealias OctopusLogEntry = OKLogEntry
 
 /// An entry in an `OKLog`.
-public struct OKLogEntry: Identifiable, Hashable, Codable, CustomStringConvertible {
+public struct OKLogEntry: Identifiable, Hashable, CustomStringConvertible {
     
     /// The title of the log, if any, in which this entry was logged.
     public let title:       String
@@ -22,15 +22,16 @@ public struct OKLogEntry: Identifiable, Hashable, Codable, CustomStringConvertib
     public let frame:       UInt64
    
     /// Specifies whether this entry was logged at the beginning of a new frame of a scene, if any. Used for highlighting new frames in a list of entries.
-    public let isNewFrame:  Bool
-    
-    public let text:        String
+    public let isNewFrame:  Bool /// CHECK: Rename to `isHighlighted` or `isFirstEntryOfNewFrame`? :p
     
     /// The file name, type name, runtime object, or subsystem from which this entry was logged.
     public let topic:       String
     
     /// The specific function or task inside the topic from which this entry is logged.
     public let function:    String
+    
+    /// The actual event or message that was logged.
+    public let text:        String
     
     /// A unique identifier for compatibility with SwiftUI lists.
     public let id         = UUID()
@@ -68,5 +69,13 @@ public struct OKLogEntry: Identifiable, Hashable, Codable, CustomStringConvertib
         let text = self.text // CHECK: Trim whitespace?
         
         return ("\(OKLog.timeFormatter.string(from: self.time))\(text.isEmpty ? "" : " ")\(text)")
+    }
+}
+
+extension OKLogEntry: Codable {
+    enum CodingKeys: String, CodingKey {
+        /// ℹ️ Exclude the long and unnecessary `id` strings.
+        case title, time, frame, isNewFrame
+        case topic, function, text
     }
 }
