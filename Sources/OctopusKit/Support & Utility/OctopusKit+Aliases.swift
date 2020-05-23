b08774f79ded759e02e9926cbc2953aaff32eada
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-24 00:59:33 +0500

    Add closure aliases and remove size alias
    Size may be used in a future integer-based type, like Point

--- Sources/OctopusKit/Support & Utility/OctopusKit+Aliases.swift
+++ Sources/OctopusKit/Support & Utility/OctopusKit+Aliases.swift
@@ -10,9 +10,13 @@
 
 // Some cleaner aliases without prefixes for commonly-used types.
 
-// NOTE: The engine API should always use the original types for specific clarity. These aliases are provided for the engine's clients.
+// NOTE: STYLE: The engine API should use the original types for specific clarity in cases where the alias may not be immediately clear.
 
 import SpriteKit
 
-public typealias Size   = CGSize
+/// A closure that takes no arguments and does not return any value.
+public typealias Closure           = () -> Void // DESIGN: It's not Closure<T> because this is meant for keeping text clutter to a minimum.
+
+public typealias NonVoidClosure<T> = () -> T
+
 public typealias Vector = CGVector
