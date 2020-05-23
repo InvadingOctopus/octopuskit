5c8e5b4848d43a6b6fec639e51937f617ddb93d1
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-23 11:45:02 +0500

    Improve BubbleComponent
    - Fix parent frames for nodes without intrinsic sizes (e.g. SKLabelNode)
    - Fix shouldBlink argument not being applied in text init
    - Reduce clutter
    - OCD indentation

--- Sources/OctopusKit/Components/Graphics/BubbleComponent.swift
+++ Sources/OctopusKit/Components/Graphics/BubbleComponent.swift
@@ -6,74 +6,73 @@
 //  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
 //
 
-// TODO: Tests
-
-// TODO: Fully customizable direction.
-
-// TODO: Queue multiple Bubbles to appear in line with one another, not overlaying and obscuring each other at once.
-
 import SpriteKit
 import GameplayKit
 
-/// Adds a child node which "floats" up from the top or bottom of the entity's `NodeComponent` node (or the specified parent.)
+/// Adds a child node which "floats" up from the top or bottom of the entity's `NodeComponent` node (or the specified parent).
 ///
 /// Useful for displaying damage values over a character etc.
 ///
 /// **Dependencies:** `NodeComponent`
-public final class BubbleComponent: NodeAttachmentComponent<SKNode> {
+public final class BubbleComponent: NodeAttachmentComponent <SKNode> {
     
-    public let bubble: SKNode
-    public let yOffset: CGFloat
-    public let spawnAtBottom: Bool
-    public let duration: TimeInterval
-    public let shouldBlink: Bool
-    public let completion: (() -> Void)?
+    // TODO: Tests
+    // TODO: Fully customizable direction.
+    // TODO: Queue multiple Bubbles to appear in line with one another, not overlaying and obscuring each other at once.
+
+    public let bubble:          SKNode
+    public let yOffset:         CGFloat
+    public let spawnAtBottom:   Bool
+    public let duration:        TimeInterval
+    public let shouldBlink:     Bool
+    public let completion:      (() -> Void)?
     
     public init(
-        bubble: SKNode,
-        yOffset: CGFloat = 0,
-        spawnAtBottom: Bool = false,
-        duration: TimeInterval = 0.5,
-        shouldBlink: Bool = true,
-        parentOverride: SKNode? = nil,
-        zPositionOverride: CGFloat = 1,
-        completion: (() -> Void)? = nil)
+        bubble:             SKNode,
+        yOffset:            CGFloat         = 0,
+        spawnAtBottom:      Bool            = false,
+        duration:           TimeInterval    = 0.5,
+        shouldBlink:        Bool            = true,
+        parentOverride:     SKNode?         = nil,
+        zPositionOverride:  CGFloat         = 1,
+        completion:         (() -> Void)?   = nil)
     {
-        self.bubble = bubble
-        self.yOffset = yOffset
-        self.spawnAtBottom = spawnAtBottom
-        self.duration = duration
-        self.shouldBlink = shouldBlink
-        self.completion = completion
-        super.init(parentOverride: parentOverride,
+        self.bubble         = bubble
+        self.yOffset        = yOffset
+        self.spawnAtBottom  = spawnAtBottom
+        self.duration       = duration
+        self.shouldBlink    = shouldBlink
+        self.completion     = completion
+        
+        super.init(parentOverride:    parentOverride,
                    zPositionOverride: zPositionOverride)
     }
     
     /// Creates a text bubble.
     public convenience init(
-        text: String,
-        font: OKFont? = nil,
-        yOffset: CGFloat = 0,
-        spawnAtBottom: Bool = false,
-        duration: TimeInterval = 0.5,
-        shouldBlink: Bool = true,
-        parentOverride: SKNode? = nil,
-        zPositionOverride: CGFloat = 1,
-        completion: (() -> Void)? = nil)
+        text:               String,
+        font:               OKFont?         = nil,
+        yOffset:            CGFloat         = 0,
+        spawnAtBottom:      Bool            = false,
+        duration:           TimeInterval    = 0.5,
+        shouldBlink:        Bool            = true,
+        parentOverride:     SKNode?         = nil,
+        zPositionOverride:  CGFloat         = 1,
+        completion:         (() -> Void)?   = nil)
     {
         let label = SKLabelNode(text: text,
                                 font: font ?? OKFont.spriteBubbleFontDefault,
                                 horizontalAlignment: .center,
                                 verticalAlignment: spawnAtBottom ? .top : .bottom)
         
-        self.init(bubble: label,
-                  yOffset: yOffset,
-                  spawnAtBottom: spawnAtBottom,
-                  duration: duration,
-                  shouldBlink: true,
-                  parentOverride: parentOverride,
-                  zPositionOverride: zPositionOverride,
-                  completion: completion)
+        self.init(bubble:               label,
+                  yOffset:              yOffset,
+                  spawnAtBottom:        spawnAtBottom,
+                  duration:             duration,
+                  shouldBlink:          shouldBlink,
+                  parentOverride:       parentOverride,
+                  zPositionOverride:    zPositionOverride,
+                  completion:           completion)
     }
     
     public required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
@@ -82,51 +81,51 @@ public final class BubbleComponent: NodeAttachmentComponent<SKNode> {
         
         // TODO: Validate parent size and account for `anchorPoint`
         
+        let parentFrame = parent.calculateAccumulatedFrame()
+        
         // Position the bubble.
         
-        bubble.position.y = spawnAtBottom ? yOffset : parent.frame.size.height + yOffset
+        bubble.position.y = spawnAtBottom ? yOffset : parentFrame.size.height + yOffset
         
         // Animate the bubble.
         
         let fadeOut = SKAction.fadeAlpha(to: 0.1, duration: duration)
         fadeOut.timingMode = .easeOut
         
-        let floatAway = SKAction.moveBy(x: 0, y: parent.frame.size.height / 2, duration: duration)
+        let floatAway = SKAction.moveBy(x: 0, y: parentFrame.size.height / 2, duration: duration)
         floatAway.timingMode = .easeOut
         
         let completion = self.completion ?? {} // Just use an empty closure for the completion callback if the `completion` property is `nil`, to avoid duplicating code below for different `run()` calls (one with the `completion:` parameter and one without.)
         
         if shouldBlink {
             
-            let hide = SKAction.hide()
-            let unhide = SKAction.unhide()
+            let hide    = SKAction.hide()
+            let unhide  = SKAction.unhide()
             
-            let blink = SKAction.sequence([
+            let blink   = SKAction.sequence([
                 hide,
-                SKAction.wait(forDuration: 0.1),
+                .wait(forDuration: 0.1),
                 unhide])
             
-            bubble.run(SKAction.sequence([
-                SKAction.wait(forDuration: 0.1),
+            bubble.run(.sequence([
+                .wait(forDuration: 0.1),
                 blink,
-                SKAction.wait(forDuration: 0.1),
+                .wait(forDuration: 0.1),
                 blink,
-                SKAction.wait(forDuration: 0.1),
-                SKAction.group([fadeOut, floatAway]),
-                SKAction.removeFromParent()]),
+                .wait(forDuration: 0.1),
+                .group([fadeOut, floatAway]),
+                .removeFromParent()]),
                        completion: completion)
-        }
-        else {
+        
+        } else {
             
-            bubble.run(SKAction.sequence([
-                SKAction.group([fadeOut, floatAway]),
-                SKAction.removeFromParent()]),
+            bubble.run(.sequence([
+                .group([fadeOut, floatAway]),
+                .removeFromParent()]),
                        completion: completion)
-            
         }
         
         return bubble
-    
     }
     
 }
