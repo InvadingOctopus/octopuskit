07bd93a45fc1b397a483f7ec8f10b55da430593a
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-24 12:19:27 +0500

    Add EntityContainerTests stub

---
+++ Tests/OctopusKitTests/Core Tests/EntityContainerTests.swift
@@ -0,0 +1,22 @@
+//
+//  EntityContainerTests.swift
+//  OctopusKitTests
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2020/05/24.
+//
+
+import XCTest
+import GameplayKit
+@testable import OctopusKit
+
+/// Tests for the core Entity Container (e.g. scene) logic.
+final class EntityContainerTests: XCTestCase {
+    
+    // MARK: - Types
+    
+    // MARK: - Tests
+    
+    static var allTests = [
+        // ("Test entity search",          testEntitySearch)
+    ]
+}
