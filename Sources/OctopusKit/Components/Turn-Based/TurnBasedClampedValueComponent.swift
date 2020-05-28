2561aa2b9cb86ced040e9434893f9aca2f9ac340
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-28 21:43:51 +0500

    Improve TurnBasedClampedValueComponent
    - Inherit from ClampedValueComponent and remove code duplication
    - Maintain TurnBased conformance
    - Eliminate redundant didSet

--- Sources/OctopusKit/Components/Turn-Based/TurnBasedClampedValueComponent.swift
+++ Sources/OctopusKit/Components/Turn-Based/TurnBasedClampedValueComponent.swift
@@ -8,18 +8,14 @@
 
 import GameplayKit
 
-/// A base class for components that represent a single numeric value within a specific range, along with a modifier that increases or decreases the value on each turn of a turn-based game.
-open class TurnBasedClampedValueComponent <ValueType> : OKTurnBasedComponent
+/// A base class for components that represent a single numeric value within a specific range, along with a modifier that increases or decreases the value on each turn of a turn-based game. Changes are reported for subclasses to act upon.
+///
+/// For example, an `EnergyComponent` may inherit from this component, implement `didChange(from:difference:)` and call a `BubbleEmitterComponent` to display changes in a character's energy.
+open class TurnBasedClampedValueComponent <ValueType> : ClampedValueComponent <ValueType>,
+    TurnBased
     where ValueType: Numeric & Comparable
 {
-    /// The value represented by this component. Setting this property will clamp it to `range`.
-    open var value: ValueType {
-        didSet { if value != oldValue { clamp() } }
-    }
-    
-    /// The acceptable range for `value`.
-    open var range: ClosedRange<ValueType>
-    
+
     /// The amount to increase or decrease `value` by on each `updateTurn(turns:)`.
     open var modifierPerTurn: ValueType
     
@@ -27,24 +23,35 @@ open class TurnBasedClampedValueComponent <ValueType> : OKTurnBasedComponent
                 initial:            ValueType,
                 modifierPerTurn:    ValueType)
     {
-        self.range              = range
-        self.value              = initial.clamped(to: range)
-        self.modifierPerTurn    = modifierPerTurn
+        self.modifierPerTurn = modifierPerTurn
         
-        super.init()
+        super.init(range:   range,
+                   initial: initial)
     }
     
     public required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
+    /// Applies the `modifierPerTurn` multiplied by the number of `turns`, to the `value`, then clamps the `value` to the `range`.
     @inlinable
-    public func clamp() {
-        value.clamp(to: range)
-    }
-    
-    @inlinable
-    open override func updateTurn(delta turns: Int = 1) {
-        value += self.modifierPerTurn * ValueType(exactly: turns)! // CHECK: Any way to avoid this `!`?
+    open func updateTurn(delta turns: Int = 1) {
+        guard modifierPerTurn != 0 else { return } /// Avoid unnecessary modifications and `didSet` triggers.
+        value += self.modifierPerTurn * ValueType(exactly: turns)! /// CHECK: Any way to avoid this `!`?
         // self.clamp() // No need to clamp here as it should be taken care of by the property's `didSet` observer.
     }
+
+    // MARK: - Abstract
+    
+    /// The following properties and methods are stubs to give the consistency of `override`, as for other subclasses of `OKTurnBasedComponent`, which this component does not inherit from, so it can avoid duplicating all the `didSet` and `didChange` code in `ClampedValueComponent`
+    //
+
+    open var disallowBeginTurn  = false
+    open var disallowUpdateTurn = false
+    open var disallowEndTurn    = false
+    
+    /// Abstract; override in subclass.
+    open func beginTurn (delta turns: Int) {}
+    
+    /// Abstract; override in subclass.
+    open func endTurn   (delta turns: Int) {}
     
 }
