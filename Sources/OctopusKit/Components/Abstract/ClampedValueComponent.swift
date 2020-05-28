f92cb050648e652bbd8122e0820fc81734a77f35
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-28 14:25:51 +0500

    Improve ClampedValueComponent
    - Add didChange(from:)
    - Improve clamping
    - Eliminate multiple didSet calls
    - BUG? APPLEBUG? 20200528A: didSet is called multiple times even if a mutating function does not modify the value.

--- Sources/OctopusKit/Components/Abstract/ClampedValueComponent.swift
+++ Sources/OctopusKit/Components/Abstract/ClampedValueComponent.swift
@@ -12,9 +12,36 @@ import GameplayKit
 open class ClampedValueComponent <ValueType> : OKComponent
     where ValueType: Numeric & Comparable
 {
-    /// The value represented by this component. Setting this property will clamp it to `range`.
+    /// DESIGN: This is not a subclass of `ValueComponent` to simplify `didSet` semantics and order of execution, and possibly also improve performance.
+    
+    // CHECK: Should there be a `willChange(to:)` or would that just be unnecessary and reduce performance?
+    
+    /// The value represented by this component. Setting this property will clamp it to `range`, then `didChange(from:)` will be called *only if* the final result is different from the previous value.
     open var value: ValueType {
-        didSet { if value != oldValue { clamp() } }
+        didSet {
+            /// ❕ NOTE: `didChange(from:)` must be called **only if** there is no change in the value **after** clamping.
+            
+            // #1: First, see if there is any difference at all in the new value assigned to the property.
+            
+            if  value != oldValue {
+                
+                // #2: Next, check if the new value be outside the range.
+                
+                /// ❕ NOTE: We perform the checks and clamping inside this observer, as calling out to other functions to perform the clamping may trigger multiple `didSet` and `didChange(from:)` events.
+                
+                // See comments in `clamp()`
+                
+                if !range.contains(value) {
+                    value = min(max(value, range.lowerBound), range.upperBound)
+                }
+                
+                // #3: Finally, let any subclasses handle the change, *if* there has actually been any change after the clamping.
+                
+                if  value != oldValue {
+                    self.didChange(from: oldValue)
+                }
+            }
+        }
     }
     
     /// The permitted range for `value`.
@@ -31,9 +58,24 @@ open class ClampedValueComponent <ValueType> : OKComponent
     
     public required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
+    /// Clamp the `value` to the `range`, if needed. If the value gets modified as a result of the clamping, `didChange(from:)` will be called.
     @inlinable
     public func clamp() {
-        value.clamp(to: range)
+        
+        /// 🐛 TODO: REPORT: BUG? 20200528A: APPLEBUG? If we call `value.clamp(to: range)`, which is a mutating function extension for `Comparable`, then the `didSet` property observer may be called multiple times, **even if** `Comparable.clamp(to:)` returns without modifying anything!
+        
+        /// To avoid that, we replicate the implementation of `Comparable.clamp(to:)` here. Who knows, this may even improve performance :P
+        
+        if !range.contains(value) {
+            value = min(max(value, range.lowerBound), range.upperBound)
+        }
     }
     
+    // MARK: - Abstract
+
+    /// Abstract; Subclasses may implement this method to respond to changes. Called by the `value` property's `didSet` observer.
+    @inlinable
+    open func didChange(from oldValue: ValueType) {}
+    
 }
+
