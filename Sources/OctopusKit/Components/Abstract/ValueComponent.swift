09558aeedc628ebde6088b534237b6e939491c03
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-05-28 10:32:47 +0500

    Add ValueComponent

---
+++ Sources/OctopusKit/Components/Abstract/ValueComponent.swift
@@ -0,0 +1,51 @@
+//
+//  ValueComponent.swift
+//  OctopusKit
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2020/05/28.
+//  Copyright © 2020 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import GameplayKit
+
+/// A base class for components that represent a single value and report its changes for subclasses to act upon.
+///
+/// For example, a subclass may call a `BubbleEmitterComponent` to display changes in the value.
+open class ValueComponent <ValueType> : OKComponent
+    where ValueType: Comparable
+{
+    /// The value represented by this component. `willChange(to:)` and `didChange(from:)` is called when this property changes to a different value.
+    open var value: ValueType {
+        
+        willSet {
+            if  newValue != value {
+                self.willChange(to: newValue)
+            }
+        }
+        
+        didSet {
+            if  value != oldValue {
+                self.didChange(from: oldValue)
+            }
+        }
+    }
+    
+    public init(initial: ValueType)
+    {
+        self.value = initial
+        super.init()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    // MARK: - Abstract
+    
+    /// Abstract; Subclasses may implement this method to respond to changes. Called by the `value` property's `willSet` observer.
+    @inlinable
+    open func willChange(to newValue: ValueType) {}
+    
+    /// Abstract; Subclasses may implement this method to respond to changes. Called by the `value` property's `didSet` observer.
+    @inlinable
+    open func didChange(from oldValue: ValueType) {}
+    
+}
