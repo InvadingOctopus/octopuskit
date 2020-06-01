ec4c72e1aac69cb7b1d78fe3d5dde3abff5947e5
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-06-01 10:11:24 +0500

    Rename SKColor.withAlpha(_:) to alpha
    To match the SwiftUI convention :)

--- Sources/OctopusKit/Apple API Extensions/SpriteKit/SKColor+OctopusKit.swift
+++ Sources/OctopusKit/Apple API Extensions/SpriteKit/SKColor+OctopusKit.swift
@@ -20,7 +20,7 @@ extension SKColor {
     /// Shorthand for `withAlphaComponent(_:)`
     ///
     /// Useful for chaining calls to `SKColor` initializers.
-    open func withAlpha(_ alpha: CGFloat) -> SKColor {
+    open func alpha(_ alpha: CGFloat) -> SKColor {
         self.withAlphaComponent(alpha)
     }
     
