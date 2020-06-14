c5826a148da4f3e7ddab35cbbb4be331bd17dfdc
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-06-14 21:33:04 +0500

    Add public access

--- Sources/OctopusKit/Core/Launch/OctopusKit+Constants.swift
+++ Sources/OctopusKit/Core/Launch/OctopusKit+Constants.swift
@@ -15,13 +15,13 @@ extension OctopusKit {
         
         /// A collection of strings for the names of entities etc., to avoid hard-coding text and prevent typos during searches or comparisons etc.
         public enum Strings {
-            static let gameCoordinatorEntityName = "Game Coordinator Entity"
+            public static let gameCoordinatorEntityName = "Game Coordinator Entity"
         }
         
         /// Various timings and durations.
         public enum Time {
             /// Represents the ideal time (in fractions of a second) per frame, in a 60 frames-per-second system: `1 / 60 = 0.01666666667`
-            static let timePerFrameIn60FPS: TimeInterval = 0.01666666667 // 1 / 60
+            public static let timePerFrameIn60FPS: TimeInterval = 0.01666666667 // 1 / 60
         }
     }
 }
