a30bc5acf704ae94746262a3b64e9e0d1392aa57
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-06-15 18:22:10 +0500

    Add removeComponents parameter

--- Sources/OctopusKit/Components/Gameplay/DelayedRemovalComponent.swift
+++ Sources/OctopusKit/Components/Gameplay/DelayedRemovalComponent.swift
@@ -8,18 +8,28 @@
 
 import GameplayKit
 
-/// Removes this component's entity from the scene after the specified time has passed.
+/// Removes this component's entity from the scene after the specified time has passed, and optionally also removes all components from the entity.
 ///
 /// If the entity's `delegate` is `nil`, then only the entity's `NodeComponent` node will be removed from its parent.
 public final class DelayedRemovalComponent: OKComponent, RequiresUpdatesPerFrame {
     
     /// The duration in seconds to wait before removing the node.
-    public var removalDelay: TimeInterval = 0
+    public var removalDelay:        TimeInterval
+   
+    /// If `true`, the entity is told to remove all its components, after it is removed from the scene.
+    public var removeComponents:    Bool = false
     
     public fileprivate(set) var secondsElapsed: TimeInterval = 0
     
-    public init(removalDelay: TimeInterval) {
-        self.removalDelay = removalDelay
+    /// Initializes a component which removes its entity from the scene.
+    /// - Parameters:
+    ///   - removalDelay:       The duration to wait before initiating the removal.
+    ///   - removeComponents:   If `true`, the entity is told to remove all its components as well, after it's removed from the scene. This may be necessary in some cases for objects to be freed from memory and `deinit`. Default: `false`
+    public init(removalDelay:       TimeInterval,
+                removeComponents:   Bool = false)
+    {
+        self.removalDelay       = removalDelay
+        self.removeComponents   = removeComponents
         super.init()
     }
     
@@ -32,8 +42,16 @@ public final class DelayedRemovalComponent: OKComponent, RequiresUpdatesPerFrame
         if  let entity = self.entity,
             secondsElapsed >= removalDelay
         {
+            #if LOGECSVERBOSE
+            OctopusKit.logForComponents("Removing \(entity)")
+            #endif
+            
             entityNode?.removeFromParent()
             entityDelegate?.entityDidRequestRemoval(entity)
+            
+            if  removeComponents {
+                entity.removeAllComponents()
+            }
         }
     }
 }
