30794ec111481faf1f85659183f4204391d63445
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2023-05-31 07:43:24 +0400

    Update package manifest
    - Update tools version to 5.8
    - Update comments to the latest Xcode 14.3 template

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 //  https://github.com/InvadingOctopus/octopuskit
@@ -14,7 +14,7 @@ let package = Package(
         .tvOS(.v16)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OctopusKit",
             targets: ["OctopusKit"]),
@@ -25,7 +25,7 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OctopusKit",
             dependencies: [],
