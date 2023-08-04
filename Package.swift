3717912244c7c00cbbea760a761ff807efd7a7b2
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2023-08-04 04:00:51 +0400

    Improve comments

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-//  https://github.com/InvadingOctopus/octopuskit
+// https://github.com/InvadingOctopus/octopuskit
 
 import PackageDescription
 
@@ -14,7 +14,7 @@ let package = Package(
         .tvOS(.v17)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OctopusKit",
             targets: ["OctopusKit"]),
@@ -24,8 +24,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "OctopusKit",
             dependencies: [],
